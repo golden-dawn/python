45448cc52e5b0667a125aa66d02798c4126396a5
Golden Dawn <golden-dawn@users.noreply.github.com>
2016-11-25 00:09:41 -0500

    Fixed issue #7: now analysis is handling properly splits. When a split is detected, the strike, in price, num contracts, range are adjuste, as well as mincall and maxput

--- analysis_8.py
+++ analysis_8.py
@@ -93,7 +93,8 @@ def check_for_pullbacks(ts, jl, sc, sgn) :
 
 
 def check_for_trend(ts, cp, min_call, max_put) :
-    print('check_for_trend: d={0:s},c={1:.2f},cp={2:s},mnc={3:.2f},mxp={4:.2f}'.format(ts.current_date(), ts.current('c'), cp, min_call, max_put))
+    print('check_for_trend: d={0:s},c={1:.2f},cp={2:s},mnc={3:.2f},mxp={4:.2f}'\
+          .format(ts.current_date(), ts.current('c'), cp, min_call, max_put))
     if cp is not None :
         if cp == 'call' :
             if ts.current('c') < min_call or min_call == -1:
@@ -128,32 +129,42 @@ def get_trade_type(ts, sc, jl_150, jl_050, pivs, lt_trend, min_call, max_put) :
         cp, min_call, max_put = check_for_trend(ts, cp, min_call, max_put)
     return cp, stp, avg_rg, min_call, max_put
 
+def get_trade_opts(stk, exp, cp, cc, rg, dt, cnx, rg_fctr = 0) :
+    # print('get_trade_opts: stk={0:s}, exp={1:s}, cp={2:s}, cc={3:.2f}, rg={4:.2f}, dt={5:s}, rg_fctr={6:.2f}'.format(stk, exp, cp, cc, rg, dt, rg_fctr));
+    if cp == "call" :
+        all_q = "select strike,dt,bid,ask from opts where exp='%s' and und=" \
+                "'%s' and strike <= %.2f and cp='call'" % \
+                (exp, ts.stk, cc + rg * rg_fctr)
+        # "'%s' and strike <= %.2f and cp='call'" % (exp, ts.stk, lns_px)
+    else :
+        all_q = "select strike,dt,bid,ask from opts where exp='%s' and und="\
+                "'%s' and strike >= %.2f and cp='put'" % \
+                (exp, ts.stk, cc - rg * rg_fctr)
+        # "'%s' and strike >= %.2f and cp='put'" % (exp, ts.stk, lns_px)
+    opt_q = "{0:s} and dt='{1:s}'".format(all_q, dt)
+    # dbg = (dt == '2004-02-12')
+    # if dbg :
+    #     print('dt={0:s}, opt_q={1:s}'.format(dt, opt_q))
+    crt_opts  = pd.read_sql(opt_q, ts.cnx)
+    opts      = pd.read_sql(all_q, ts.cnx)
+    strike    = crt_opts['strike'].max() if cp == 'call' else \
+                crt_opts['strike'].min()
+    # if dbg :
+    #     print('dt={0:s}, strike={1:2f}'.format(dt, strike))
+    trd_opts  = opts[opts['strike']==strike]
+    t_opts    = collections.OrderedDict()
+    for row in trd_opts.iterrows() :
+        t_opts[row[1][1]] = [row[1].bid, row[1].ask]
+    # if dbg :
+    #     print(t_opts)
+    return t_opts, strike
 
 def trade(ts, sc, cp, sl, rg, stp, ind_ranks) :
     crt_dt    = ts.current_date()
     cc        = ts.current('c')
     exp       = str(sc.next_expiry(crt_dt, 15))
     exp_bd    = str(sc.prev_busday(sc.move_busdays(exp, 0)).date())
-    if cp == "call" :
-        all_q = "select strike,dt,bid,ask from opts where exp='%s' and und=" \
-                "'%s' and strike <= %.2f and cp='call'" % (exp, ts.stk, cc)
-        # "'%s' and strike <= %.2f and cp='call'" % (exp, ts.stk, lns_px)
-    else :
-        all_q = "select strike,dt,bid,ask from opts where exp='%s' and und="\
-                "'%s' and strike >= %.2f and cp='put'" % (exp, ts.stk, cc)
-        # "'%s' and strike >= %.2f and cp='put'" % (exp, ts.stk, lns_px)
-    opt_q = "{0:s} and dt='{1:s}'".format(all_q, crt_dt)
-    # print('crt_dt={0:s}, opt_q={1:s}'.format(crt_dt, opt_q))
-    crt_opts  = pd.read_sql(opt_q, ts.cnx)
-    opts      = pd.read_sql(all_q, ts.cnx)
-    strike    = crt_opts['strike'].max() if cp == 'call' else \
-                crt_opts['strike'].min()
-    # print('strike={0:s}, strike={1:2f}'.format(crt_dt, strike))
-    trd_opts  = opts[opts['strike']==strike]
-    t_opts    = collections.OrderedDict()
-    for row in trd_opts.iterrows() :
-        t_opts[row[1][1]] = [row[1].bid, row[1].ask]
-    # print(t_opts)
+    t_opts, strike = get_trade_opts(ts.stk, exp, cp, cc, rg, crt_dt, ts.cnx)
     if t_opts.get(crt_dt) is None or t_opts[crt_dt][1] == 0:
         return None
     num       = int(6 / t_opts[crt_dt][1]) * 100
@@ -161,13 +172,11 @@ def trade(ts, sc, cp, sl, rg, stp, ind_ranks) :
     return Trade(ts.stk, cp, exp, exp_bd, strike, opt_in, num, sl, None,
                  None, t_opts, stp, ind_ranks)
 
-
 def to_short_string(trd) :
     return '  {0:s} {1:s} {2:s} {3:s} {4:.2f} {5:s}'.\
         format(trd.stk, trd.opt_in.dt, trd.cp, trd.exp, trd.strike, trd.exp_bd)
 
-
-def risk_mgmt(ts, open_trades, trades) :
+def risk_mgmt(sc, ts, open_trades, trades) :
     crt_dt                   = ts.current_date()
     closed_trades            = []
     for trd in open_trades :
@@ -195,13 +204,12 @@ def risk_mgmt(ts, open_trades, trades) :
                          '{0:.2f}'.format(pnl_pct) if success == 0 else '',
                          '{0:.2f}'.format(pnl) if success == 1 else '',
                          '{0:.2f}'.format(pnl) if success == 0 else '')
-            trd.opt_stats = opt_stats
-            trd.opt_out   = opt_out
+            trd.opt_stats    = opt_stats
+            trd.opt_out      = opt_out
             closed_trades.append(trd)
     for trd in closed_trades :
         open_trades.remove(trd)
         trades.append(trd)
-
 
 def to_csv_list(trd) :
     return [trd.stk, trd.opt_in.dt, trd.opt_in.dt[:4], trd.cp, trd.exp,
@@ -249,68 +257,86 @@ def get_trend(ts, jl, pivs) :
 
 
 def analyze(ts, sc, calls, puts, fname, fmode) :
-    trades          = []
-    open_trades     = []
-    min_call        = -1
-    max_put         = -1
+    trades           = []
+    open_trades      = []
+    min_call         = -1
+    max_put          = -1
     for gap in ts.gaps:
-        start, end  = gap[0], gap[1]
+        start, end   = gap[0], gap[1]
         ts.set_day(str(start.date()))
-        ldr_df      = ts.df[start:end].query('ldr==1')
+        ldr_df       = ts.df[start:end].query('ldr==1')
         # print('len(ldr_df) = {0:d}'.format(len(ldr_df)))
         if len(ldr_df) == 0 :
             continue       
-        jl_150      = StxJL(ts, 1.50)
-        jl_050      = StxJL(ts, 0.50)
+        jl_150       = StxJL(ts, 1.50)
+        jl_050       = StxJL(ts, 0.50)
         ts.set_day(str(ts.df.index[ts.start + 20].date()))
-        start_w     = jl_150.initjl()
-        start_w     = jl_050.initjl()
+        start_w      = jl_150.initjl()
+        start_w      = jl_050.initjl()
         # print('start_w = {0:d}, jl_150.w = {1:d}'.format(start_w, jl_150.w))
-        ixx         = start_w if start_w >= jl_150.w else -1
-        lt_trend    = 0
+        ixx          = start_w if start_w >= jl_150.w else -1
+        lt_trend     = 0
         while ixx != -1 :
-            ixx     = ts.next_day()
+            ixx      = ts.next_day()
+            crt_dt   = ts.current_date()
             if ixx == -1 :
                 break
             jl_150.nextjl()
             jl_050.nextjl()
-            pivs    = jl_150.get_pivots_in_days(400)
-            risk_mgmt(ts, open_trades, trades)
+            pivs     = jl_150.get_pivots_in_days(400)
+            risk_mgmt(sc, ts, open_trades, trades)
             # for piv in pivs(
             lt_trend = get_trend(ts, jl_150, pivs)
-            # print('{0:s}: lt_trend={1:d}'.format(ts.current_date(), lt_trend))
+            # print('{0:s}: lt_trend={1:d}'.format(crt_dt, lt_trend))
             cp, stp, sl, min_call, max_put = get_trade_type \
                                              (ts, sc, jl_150, jl_050, pivs,
                                               lt_trend, min_call, max_put)
-            if cp is None :
-                continue
-            print('{0:s} {1:s} {2:s}'.format(ts.current_date(), cp, stp))
-            trades_by_exp = calls if cp == 'call' else puts
-            stk_trades_by_exp = trades_by_exp.get(ts.stk)
-            if stk_trades_by_exp is None :
-                stk_trades_by_exp = {}
-                trades_by_exp[stk] = stk_trades_by_exp
-            exp          = str(sc.next_expiry(ts.current_date(), 15))
-            open_trades_by_exp = stk_trades_by_exp.get(exp)
-            if open_trades_by_exp is not None :
-                print('{0:s}: Trade already there {1:s}'.\
-                      format(ts.current_date(),
-                             to_short_string(open_trades_by_exp)))
-                continue
-            ind_ranks    = IndRanks(ts.current('r_rs_252'),
-                                    ts.current('udv_21b'),
-                                    ts.current('udv_42b'),
-                                    ts.current('udv_63b'),
-                                    ts.current('rs_21b'),
-                                    ts.current('rs_min_252'),
-                                    ts.current('rs_min_252b'))
-            trd          = trade(ts, sc, cp, sl, jl_150.last_rec('rg'), stp,
-                                 ind_ranks)
-            if trd is not None :
-                open_trades.append(trd)
-                stk_trades_by_exp[exp] = trd
-                trades_by_exp[ts.stk]  = stk_trades_by_exp
-                print('New Trade: {0:s}'.format(to_short_string(trd)))
+            if cp is not None :
+                print('{0:s} {1:s} {2:s}'.format(crt_dt, cp, stp))
+                trades_by_exp          = calls if cp == 'call' else puts
+                stk_trades_by_exp      = trades_by_exp.get(ts.stk)
+                if stk_trades_by_exp is None :
+                    stk_trades_by_exp  = {}
+                    trades_by_exp[stk] = stk_trades_by_exp
+                exp          = str(sc.next_expiry(crt_dt, 15))
+                open_trades_by_exp = stk_trades_by_exp.get(exp)
+                if open_trades_by_exp is not None :
+                    print('{0:s}: Trade already there {1:s}'.\
+                          format(crt_dt, to_short_string(open_trades_by_exp)))
+                else :
+                    ind_ranks    = IndRanks(ts.current('r_rs_252'),
+                                            ts.current('udv_21b'),
+                                            ts.current('udv_42b'),
+                                            ts.current('udv_63b'),
+                                            ts.current('rs_21b'),
+                                            ts.current('rs_min_252'),
+                                            ts.current('rs_min_252b'))
+                    trd          = trade(ts, sc, cp, sl, jl_150.last_rec('rg'),
+                                         stp, ind_ranks)
+                    if trd is not None :
+                        open_trades.append(trd)
+                        stk_trades_by_exp[exp] = trd
+                        trades_by_exp[ts.stk]  = stk_trades_by_exp
+                        print('New Trade: {0:s}'.format(to_short_string(trd)))
+            ratio                      = ts.splits.get(pd.Timestamp(crt_dt))
+            # print('dt={0:s}, ratio={1:s}'.format(crt_dt, str(ratio)))
+            if ratio is not None :
+                next_bd                = str(sc.next_busday(crt_dt).date())
+                # print('FOUND SPLIT: dt={0:s}, ratio={1:.2f}, next_bd={2:s}'.\
+                #       format(crt_dt, ratio, next_bd))
+                min_call               = min_call * ratio
+                max_put                = max_put * ratio
+                for trd in open_trades :
+                    trd.strike         = trd.strike * ratio
+                    trd.opt_in.spot_px = trd.opt_in.spot_px * ratio
+                    trd.opt_in.opt_px  = trd.opt_in.opt_px *  ratio
+                    trd.num            = trd.num / ratio
+                    trd.avg_range      = trd.avg_range * ratio
+                    trd.opts, strike   = get_trade_opts \
+                                         (ts.stk, trd.exp, trd.cp, \
+                                          trd.strike, trd.avg_range, next_bd, \
+                                          ts.cnx, 0.1 / trd.avg_range)
+                    # print(trd.opts)
     with open(fname, fmode, newline='') as fp :
         wrtr = csv.writer(fp, delimiter=',')
         if fmode == 'w' :
