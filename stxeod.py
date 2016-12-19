42b62978a851643186f577fc858a0cd56d91bfa0
Golden Dawn <golden-dawn@users.noreply.github.com>
2016-12-19 03:58:00 -0500

    WIP: modified stxeod.py to work with the new stxdb.py implementation

--- stxeod.py
+++ stxeod.py
@@ -1,55 +1,46 @@
 import os
-import stxdb
+from stxdb import *
 import sys
 
 class StxEOD :
 
-    upload_dir = 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads'
-    eod_name   = '{0:s}/eod_upload.txt'.format(upload_dir)
-
-    def __init__(self, cnx, in_dir, eod_tbl, split_tbl, extension = '.txt') :
+    upload_dir       = 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads'
+    eod_name         = '{0:s}/eod_upload.txt'.format(upload_dir)
+    sql_create_eod   = 'CREATE TABLE `{0:s}` ('\
+                       '`stk` varchar(8) NOT NULL,'\
+                       '`dt` varchar(10) NOT NULL,'\
+                       '`o` decimal(9,2) DEFAULT NULL,'\
+                       '`h` decimal(9,2) DEFAULT NULL,'\
+                       '`l` decimal(9,2) DEFAULT NULL,'\
+                       '`c` decimal(9,2) DEFAULT NULL,'\
+                       '`v` int(11) DEFAULT NULL,'\
+                       'PRIMARY KEY (`stk`,`dt`)'\
+                       ') ENGINE=MyISAM DEFAULT CHARSET=utf8'
+    sql_create_split = 'CREATE TABLE `{0:s}` ('\
+                       '`stk` varchar(8) NOT NULL,'\
+                       '`dt` varchar(10) NOT NULL,'\
+                       '`ratio` decimal(8,4) DEFAULT NULL,'\
+                       'PRIMARY KEY (`stk`,`dt`)'\
+                       ') ENGINE=MyISAM DEFAULT CHARSET=utf8'
+    
+    def __init__(self, in_dir, eod_tbl, split_tbl, extension = '.txt') :
         self.in_dir    = in_dir
         self.eod_tbl   = eod_tbl
         self.split_tbl = split_tbl
         self.extension = extension
-        self.cnx       = cnx
-        if cnx.query("show tables like '{0:s}'".format(eod_tbl)) == 0 :
-            try :
-                cnx.query('CREATE TABLE `{0:s}` ('\
-                          '`stk` varchar(8) NOT NULL,'\
-                          '`dt` varchar(10) NOT NULL,'\
-                          '`o` decimal(9,2) DEFAULT NULL,'\
-                          '`h` decimal(9,2) DEFAULT NULL,'\
-                          '`l` decimal(9,2) DEFAULT NULL,'\
-                          '`c` decimal(9,2) DEFAULT NULL,'\
-                          '`v` int(11) DEFAULT NULL,'\
-                          'PRIMARY KEY (`stk`,`dt`)'\
-                          ') ENGINE=MyISAM DEFAULT CHARSET=utf8'.\
-                          format(eod_tbl))
-                print('Successfully created DB table {0:s}'.format(eod_tbl))
-            except :
-                e = sys.exc_info()[1]
-                print('Failed to create DB table {0:s}, error {1:s}'.\
-                      format(eod_tbl, str(e)))
-                raise
-        if cnx.query("show tables like '{0:s}'".format(split_tbl)) == 0 :
-            try :
-                cnx.query('CREATE TABLE `{0:s}` ('\
-                          '`stk` varchar(8) NOT NULL,'\
-                          '`dt` varchar(10) NOT NULL,'\
-                          '`ratio` decimal(8,4) DEFAULT NULL,'\
-                          'PRIMARY KEY (`stk`,`dt`)'\
-                          ') ENGINE=MyISAM DEFAULT CHARSET=utf8'.\
-                          format(split_tbl))
-                print('Successfully created DB table {0:s}'.format(split_tbl))
-            except :
-                e = sys.exc_info()[1]
-                print('Failed to create DB table {0:s}, error {1:s}'.\
-                      format(split_tbl, str(e)))
-                raise
+        db_create_missing_table(eod_tbl, cls.sql_create_eod)
+        print('EOD DB table: {0:s}'.format(eod_tbl))
+        db_create_missing_table(split_tbl, cls.sql_create_split)
+        print('Split DB table: {0:s}'.format(split_tbl))
 
-
-    def load_from_files(self, stx = '') :
+        
+    def load_ctin_files(self, stx = '') :
+        split_fname  = '{0:s}/splits_ctin.txt'.format(cls.upload_dir)
+        try :
+            os.remove(split_fname)
+            print('Removed {0:s}'.format(split_fname))
+        except :
+            pass
         if stx == '' :
             lst      = [f for f in os.listdir(self.in_dir) \
                         if f.endswith(self.extension)]
@@ -62,40 +53,20 @@ class StxEOD :
         print('Loading data for {0:d} stocks'.format(num_stx))
         ixx          = 0
         for fname in lst :
-            self.load_stk(fname)
+            self.load_ctin_stk(fname, split_fname)
             ixx     += 1
             if ixx % 500 == 0 or ixx == num_stx :
                 print('Uploaded {0:5d}/{1:5d} stocks'.format(ixx, num_stx))
-
-
-    def reload_from_files(self, stx = '') :
-        if stx == '' :
-            lst        = [f for f in os.listdir(self.in_dir) \
-                          if f.endswith(self.extension)]
-        else :
-            lst      = []
-            stk_list = stx.split(',')
-            for stk in stk_list :
-                lst.append('{0:s}{1:s}'.format(stk.strip(), self.extension))
-        num_stx    = len(lst)
-        print('Reloading data for {0:d} stocks'.format(num_stx))
-        ixx        = 0
-        reloaded   = 0
-        cursor     = self.cnx.cursor()
-        for fname in lst :
-            cursor.execute("select count(*) from {0:s} where stk='{1:s}'".\
-                           format(self.eod_tbl, fname[:-4]))
-            for n in cursor :
-                if n[0] == 0 :
-                    self.repair_file(fname)
-                    self.load_stk(fname)
-            ixx   += 1
-            if ixx % 500 == 0 or ixx == num_stx :
-                print('Reloaded {0:5d}/{1:5d} stocks'.format(ixx, num_stx))
-        cursor.close()
-
-                
-    def load_stk(self, short_fname) :
+        try :
+            db_upload_file(split_fname, self.split_tbl, 2)
+            print('Successfully uploaded the splits in the DB')
+        except :
+            e = sys.exc_info()[1]
+            print('Failed to upload the splits from file {0:s}, error {1:s}'.\
+                  format(split_fname, str(e)))
+            
+            
+    def load_ctin_stk(self, short_fname, split_fname) :
         fname = '{0:s}/{1:s}'.format(self.in_dir, short_fname)
         stk   = short_fname[:-4]
         try :
@@ -106,7 +77,6 @@ class StxEOD :
             print('Failed to read {0:s}, error {1:s}'.\
                   format(short_fname, str(e)))
             return
-        q_split = 'insert into {0:s} values'.format(self.split_tbl)
         eods, splits = 0, 0
         try :
             with open(self.eod_name, 'w') as eod :
@@ -119,42 +89,25 @@ class StxEOD :
                                      tokens[3], tokens[4], tokens[5]))
                     eods += 1
                     if start == 1 :
-                        sep = ' ' if splits == 0 else ','
                         splits += 1
-                        q_split = "{0:s}{1:s}('{2:s}','{3:s}',{4:f})".format \
-                            (q_split, sep, stk, tokens[0], float(tokens[6]))
+                        with open(split_fname, 'a') as split_file :
+                            split_file.write('{0:s}\t{1:s}\t{2:f}\n'.format \
+                                             (stk, tokens[0], float(tokens[6])))
         except :
             e = sys.exc_info()[1]
             print('Failed to parse {0:s}, error {1:s}'.\
                   format(short_fname, str(e)))
             return
         try :
-            self.cnx.query("load data infile '{0:s}' into table {1:s}". \
-                           format(self.eod_name, self.eod_tbl))
+            db_upload_file(self.eod_name, self.eod_tbl, 2)
+            print('{0:s}: uploaded {1:d} eods and {2:d} splits'.\
+                  format(stk, eods, splits))
         except :
             e = sys.exc_info()[1]
             print('Failed to upload {0:s}, error {1:s}'.\
                   format(short_fname, str(e)))
-            return
-        if splits > 0 :
-            try :
-                self.cnx.query(q_split)
-            except :
-                e = sys.exc_info()[1]
-                print('Failed to upload {0:s}, error {1:s}'.\
-                      format(short_fname, str(e)))
-        print('{0:s}: uploaded {1:d} eods and {2:d} splits'.\
-              format(stk, eods, splits))
 
-    def repair_file(self, fname) :
-        # This function removes duplicate entries and incorrect rows
-        # VPA, WEG: duplicate entries
-        # VOXXE, WEFND: invalid utf8 character
-        # QQQQ: list index out of range
-        # Failed to upload LDX.txt, error (1264, "Out of range value for column 'ratio' at row 1")
-        pass
         
 if __name__ == '__main__' :
-    cnx  = stxdb.connect()
-    seod = StxEOD(cnx, 'c:/goldendawn/bkp', 'eod1', 'split1')
-    seod.load_from_files('XTR')
+    seod = StxEOD('c:/goldendawn/bkp', 'eod1', 'split1')
+    seod.load_ctin_files('XTR')
