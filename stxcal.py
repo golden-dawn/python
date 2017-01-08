bdd7cfd5902c2b62a48e54c05fa8459fb31c7451
Golden Dawn <golden-dawn@users.noreply.github.com>
2017-01-08 03:28:00 -0500

    Reformatted stxcal.py to follow FlyCheck syntax guidelines

--- stxcal.py
+++ stxcal.py
@@ -9,11 +9,13 @@ from dateutil import rrule
 this = sys.modules[__name__]
 this.cal = None
 
-def get_cal() :
-    if this.cal is None :
+
+def get_cal():
+    if this.cal is None:
         start = pd.Timestamp('1998-01-01', tz='UTC')
-        end = pd.Timestamp('today', tz='UTC') + pd.datetools.Timedelta(weeks=52)
-        print('Initializing calendar between {0:s} and {1:s}'.\
+        end = pd.Timestamp('today', tz='UTC') + \
+            pd.datetools.Timedelta(weeks=52)
+        print('Initializing calendar between {0:s} and {1:s}'.
               format(str(start.date()), str(end.date())))
         non_trading_rules = []
         new_years = rrule.rrule(
@@ -173,28 +175,32 @@ def get_cal() :
         for non_trading_day in non_trading_days:
             if non_trading_day.weekday() < 5:
                 hols.append(str(non_trading_day.date()))
-        hols.append('2001-09-11') # September 11, 2001
-        hols.append('2001-09-12') # September 11, 2001
-        hols.append('2001-09-13') # September 11, 2001
-        hols.append('2001-09-14') # September 11, 2001
-        hols.append('2004-06-11') # Reagan's funeral
-        hols.append('2007-01-02') # Ford's funeral
-        hols.append('2012-10-29') # Frankenstorm
-        hols.append('2012-10-30') # Frankenstorm
+        hols.append('2001-09-11')  # September 11, 2001
+        hols.append('2001-09-12')  # September 11, 2001
+        hols.append('2001-09-13')  # September 11, 2001
+        hols.append('2001-09-14')  # September 11, 2001
+        hols.append('2004-06-11')  # Reagan's funeral
+        hols.append('2007-01-02')  # Ford's funeral
+        hols.append('2012-10-29')  # Frankenstorm
+        hols.append('2012-10-30')  # Frankenstorm
         hols.sort()
-        this.cal = np.busdaycalendar(holidays = hols)
+        this.cal = np.busdaycalendar(holidays=hols)
     return this.cal
-        
-def next_busday(dt) :
-    return str(pd.to_datetime(np.busday_offset(dt, 1, roll='backward', \
+
+
+def next_busday(dt):
+    return str(pd.to_datetime(np.busday_offset(dt, 1, roll='backward',
                                                busdaycal=get_cal())).date())
 
-def prev_busday(dt) :
-    return str(pd.to_datetime(np.busday_offset(dt, -1, roll='forward', \
+
+def prev_busday(dt):
+    return str(pd.to_datetime(np.busday_offset(dt, -1, roll='forward',
                                                busdaycal=get_cal())).date())
+
 
 def is_busday(dt):
     return np.is_busday(dt, busdaycal=get_cal())
+
 
 # Move backwards (if bdays < 0) or forwards (if bdays > 0) bdays
 # business days.  If bdays is 0, then will not move if dt is already a
@@ -205,10 +211,12 @@ def is_busday(dt):
 # Friday afterwards.
 def move_busdays(dt, bdays):
     return str(np.busday_offset(dt, bdays, busdaycal=get_cal(),
-                                roll='forward' if bdays<0 else 'backward'))
+                                roll='forward' if bdays < 0 else 'backward'))
+
 
 def num_busdays(sdt, edt):
     return np.busday_count(sdt, edt, busdaycal=get_cal())
+
 
 def expiry(dt):
     sat_fri_date = np.datetime64('2015-02')
@@ -217,32 +225,34 @@ def expiry(dt):
         exp = np.busday_offset(exp, 1, weekmask='1111111')
     return str(exp)
 
+
 # call it like this: expiries('2015-01', '2015-08')
 def expiries(sdt, edt):
     return [expiry(dt) for dt in np.arange(sdt, edt, dtype='datetime64[M]')]
 
+
 def next_expiry(dt, min_days=1):
-    ym           = np.datetime64(str(dt)[ :-3])
-    last_exp     = move_busdays(expiry(ym), 0)
-    while num_busdays(dt, last_exp) < min_days :
-        ym      += np.timedelta64(1, 'M')
+    ym = np.datetime64(str(dt)[:-3])
+    last_exp = move_busdays(expiry(ym), 0)
+    while num_busdays(dt, last_exp) < min_days:
+        ym += np.timedelta64(1, 'M')
         last_exp = move_busdays(expiry(ym), 0)
     return expiry(ym)
+
 
 def prev_expiry(dt, min_days=0):
-    ym           = np.datetime64(str(dt)[ :-3])
-    last_exp     = move_busdays(expiry(ym), 0)
-    while num_busdays(last_exp, dt) < min_days :
-        ym      -= np.timedelta64(1, 'M')
+    ym = np.datetime64(str(dt)[:-3])
+    last_exp = move_busdays(expiry(ym), 0)
+    while num_busdays(last_exp, dt) < min_days:
+        ym -= np.timedelta64(1, 'M')
         last_exp = move_busdays(expiry(ym), 0)
     return expiry(ym)
 
-def long_expiries(starting_from = None) :
+
+def long_expiries(starting_from=None):
     s_date = datetime.strftime(datetime.now(), '%Y-%m-%d')\
              if starting_from is None else starting_from
     e_date = next_expiry(s_date, 8 * 252 // 12)
-    epoch  = datetime.utcfromtimestamp(0)
-    return [int((datetime.strptime(x, '%Y-%m-%d') - epoch).total_seconds()) \
+    epoch = datetime.utcfromtimestamp(0)
+    return [int((datetime.strptime(x, '%Y-%m-%d') - epoch).total_seconds())
             for x in expiries(s_date[:-3], e_date[:-3])]
-    
-    
