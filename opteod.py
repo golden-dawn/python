bdc0b881b30be3101b2edded0bb40f1ddbaa8b05
Golden Dawn <golden-dawn@users.noreply.github.com>
2017-02-04 04:03:28 -0500

    Added TODOs needed to wrap up this item

--- opteod.py
+++ opteod.py
@@ -44,6 +44,7 @@ class OptEOD:
 
     def load_opts_archive(self, zip_fname):
         tmp_dir = '{0:s}/tmp'.format(self.in_dir)
+        # TODO: remove the tmp directory (if it already exists, and recreate it
         print('Unzipping {0:s} into {1:s}'.format(zip_fname, tmp_dir))
         with zipfile.ZipFile(zip_fname, 'r') as zip_file:
             zip_file.extractall(tmp_dir)
@@ -53,8 +54,11 @@ class OptEOD:
             opt_df = pd.read_csv(os.path.join(tmp_dir, opt_file),
                                  parse_dates=['Expiration', 'DataDate'],
                                  infer_datetime_format='%m/%d/%Y')
+            # TODO: round the strike and the spot to 2 significant digits
 
             # replace 'call' and 'put' with 'c' and 'p'
+            # TODO: apply the function to the cp column (not Type),
+            # after renaming the column
             def cpfun(x):
                 return x['Type'][:1]
             opt_df['cp'] = opt_df.apply(cpfun, axis=1)
@@ -63,6 +67,10 @@ class OptEOD:
                         inplace=True)
             opt_df.columns = ['und', 'spot', 'exp', 'dt', 'strike',
                               'bid', 'ask', 'cp']
+            # TODO: calculate the next 6 monthly expiries, and filter
+            # the dataframe to only include those expiries
+            # TODO: round the strikes and spots, as well as change
+            # call and put after filtering
             spot_df = opt_df[['und', 'spot', 'dt']].drop_duplicates()
             spot_df.columns = ['stk', 'spot', 'dt']
             spot_df.set_index(['stk', 'dt'], inplace=True)
@@ -73,3 +81,5 @@ class OptEOD:
                              inplace=True)
             opt_df.to_sql(self.opt_tbl, stxdb.db_get_cnx(), flavor='mysql',
                           if_exists='append')
+        # TODO: remove the tmp directory after we are done with the
+        # current archive
