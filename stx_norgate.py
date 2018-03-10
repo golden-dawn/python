87f4e9627471b28feb5507001f8f1fbc9a495b5d
golden-dawn <constantin_m_adam@yahoo.com>
2018-03-10 03:12:20 -0500

    WIP: added dataframes, splits handling

--- stx_norgate.py
+++ stx_norgate.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import pandas as pd
 import subprocess
 import stxdb
@@ -39,8 +40,8 @@ class StxNorgate:
                 self.process_data(sdir)
 
     def process_data(self, in_dir):
-        self.upload_names(in_dir)
-        self.upload_prices(in_dir)
+        stx = self.upload_names(in_dir)
+        self.upload_prices(in_dir, sorted(stx))
 
     def upload_names(self, in_dir):
         with open('{0:s}/names.txt'.format(self.upload_dir), 'r') as f:
@@ -53,10 +54,42 @@ class StxNorgate:
             stxdb.db_write_cmd(
                 "INSERT INTO equities VALUES ('{0:s}', '{1:s}', 'US Stocks', "
                 "'US') on conflict (stk) do nothing".format(ticker, name))
+        return dct.keys()
 
-    def upload_prices(self, in_dir):
+    def upload_prices(self, in_dir, stx):
         df = pd.read_csv('{0:s}/prices.txt', sep='\t', header=0)
+        for stk in stx:
+            stk_df = df.query("symbol=='{0:s}'".format(stk))
+            stk_df, splits = self.get_splits(stk_df)
+
+    def get_splits(self, df):
+        df['r'] = df['close'] / df['openint']
+        df['r_1'] = df['r'].shift()
+        df.r_1.fillna(df.r, inplace=True)
+        df['rr'] = np.round(df['r'] / df['r_1'], 4)
+        df['rr_1'] = df['rr'].shift(-1)
+        df.rr_1.fillna(df.rr, inplace=True)
+        splits = df.query('rr_1<0.999 | rr_1>1.001')
+        split_dct = {}
+        for ixx, row in splits.iterrows():
+            ratio = 1 / row['rr_1']
+            split_dct[row['date']] = ratio
+            df.loc[0:ixx, 'open'] = np.round(df['open'] / ratio, 2)
+            df.loc[0:ixx, 'high'] = np.round(df['high'] / ratio, 2)
+            df.loc[0:ixx, 'low'] = np.round(df['low'] / ratio, 2)
+            df.loc[0:ixx, 'close'] = np.round(df['close'] / ratio, 2)
+            df.loc[0:ixx, 'volume'] = np.round(df['volume'] * ratio, 0)
+        df.drop(['r', 'r_1', 'rr', 'rr_1'], axis=1, inplace=True)
+        return df, split_dct
 
 
 if __name__ == '__main__':
     pass
+'''
+from stx_norgate import StxNorgate
+sn = StxNorgate()
+import pandas as pd
+df_g = pd.read_csv('/tmp/prices_g.txt', sep='\t', header=0)
+df = df_g.query("symbol=='GGG'")
+res, splits = sn.get_splits(df)
+'''
