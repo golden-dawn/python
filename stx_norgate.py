aadae0c40d3d696fc2754e83c427e0fa9d0a006d
golden-dawn <constantin_m_adam@yahoo.com>
2018-03-23 21:38:20 -0400

    Fixed bug - check for length of parameters before selecting first

--- stx_norgate.py
+++ stx_norgate.py
@@ -111,7 +111,7 @@ class StxNorgate:
 
 
 if __name__ == '__main__':
-    if sys.argv[1] == 'test':
+    if len(sys.argv) > 1 and sys.argv[1] == 'test':
         test = True
     else:
         test = False
