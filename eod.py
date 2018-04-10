200cb8b3ec5c96eff22f7c06e9b57c396143de4e
golden-dawn <constantin_m_adam@yahoo.com>
2018-04-10 01:51:39 -0400

    Added pseudocode to get eod data to eod.py

--- eod.py
+++ eod.py
@@ -21,3 +21,11 @@ class Test1EOD(unittest.TestCase):
         eod.parseeodfiles(self.sd, self.ed)
         eod.eod_reconciliation('', self.sd, self.ed)
         self.assertTrue(1 == 1)
+
+import requests
+r = requests.get('https://finance.yahoo.com/quote/GBTC/history?p=GBTC')
+start_idx = r.text.find('HistoricalPriceStore') + 33
+end_idx = r.text.find('isPending') - 2
+with open('../../gbtc_test.txt', 'w') as f:
+    f.write(r.text[start_idx: end_idx])
+r = requests.get('https://stooq.com/q/d/l?s=C.F&d1=20180320&d2=20180402')
