9928657780448cfd40ecbfba2cc5714864e0821d
golden-dawn <constantin_m_adam@yahoo.com>
2018-04-13 00:49:03 -0400

    Properly initialize the JL algorithm

--- jlml.py
+++ jlml.py
@@ -9,11 +9,15 @@ ed = '2018-03-09'
 factor = 1.5
 ts = StxTS(stk, sd, ed)
 jl = StxJL(ts, factor)
+jl_start = jl.w + ts.start
+ts.set_day(str(ts.ix(jl_start).name.date()))
 start_w = jl.initjl()
 for ixx in range(start_w, ts.end + 1):
     ts.next_day()
     jl.nextjl()
     pivs = jl.get_num_pivots(4)
-    print("{0:s}: avg range: {1:.2f}; 4 pivs:".format(ts.current_date(),
-                                                      jl.avg_rg))
+    print('{0:s} {1:.2f} {2:.2f} {3:.2f} {4:.2f} {5:.0f}, avg rg: {6:.2f}; '
+          '4 pivs:'.format(ts.current_date(), ts.current('o'),
+                           ts.current('hi'), ts.current('lo'),
+                           ts.current('c'), ts.current('volume'), jl.avg_rg))
     jl.print_pivs(pivs)
