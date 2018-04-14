7dea0622f8e3922535f7396695596ad7fb1e0120
golden-dawn <constantin_m_adam@yahoo.com>
2018-04-14 02:57:52 -0400

    WIP: generate data needed for ML

--- jlml.py
+++ jlml.py
@@ -1,23 +1,44 @@
+import math
 import stxcal
-import sys
-from stxts import StxTS
+import stxdb
 from stxjl import StxJL
+from stxts import StxTS
 
-stk = 'NFLX'
-sd = '2002-02-01'
-ed = '2018-03-09'
-factor = 1.5
-ts = StxTS(stk, sd, ed)
-jl = StxJL(ts, factor)
-jl_start = jl.w + ts.start
-ts.set_day(str(ts.ix(jl_start).name.date()))
-start_w = jl.initjl()
-for ixx in range(start_w, ts.end + 1):
-    ts.next_day()
-    jl.nextjl()
-    pivs = jl.get_num_pivots(4)
-    print('{0:s} {1:.2f} {2:.2f} {3:.2f} {4:.2f} {5:.0f}, avg rg: {6:.2f}; '
-          '4 pivs:'.format(ts.current_date(), ts.current('o'),
-                           ts.current('hi'), ts.current('lo'),
-                           ts.current('c'), ts.current('volume'), jl.avg_rg))
-    jl.print_pivs(pivs)
+
+class JLML:
+    def __init__(self):
+        self.sd = '1950-01-01'
+        self.ed = '2020-12-31'
+        self.factors = [0.6, 1.1, 1.6]
+
+    def gen_stk_data(self, stk):
+        ts = StxTS(stk, self.sd, self.ed)
+        for factor in self.factors:
+            jl = StxJL(ts, factor)
+            jl_start = jl.w + ts.start
+            ts.set_day(str(ts.ix(jl_start).name.date()))
+            start_w = jl.initjl()
+            for ixx in range(start_w, ts.end + 1):
+                ts.next_day()
+                jl.nextjl()
+                pivs = jl.get_num_pivots(4)
+                print('{0:s} {1:.2f} {2:.2f} {3:.2f} {4:.2f} {5:.0f} '
+                      'rg: {6:.2f} {7:s}'.format(
+                          ts.current_date(), ts.current('o'), ts.current('hi'),
+                          ts.current('lo'), ts.current('c'),
+                          ts.current('volume'), jl.avg_rg,
+                          self.print_stats(ts, pivs)))
+
+    def print_stats(self, jl, ts, pivs):
+        piv_data = []
+        cc = ts.current('c')
+        for piv in reversed(pivs):
+            num_days = stxcal.num_busdays(piv.dt, ts.current_date())
+            dist = (piv.price - cc) / (jl.avg_rg * math.sqrt(num_days))
+
+    def up_down_volume(self, ts, piv):
+        total_volume = 0
+        signed_volume = 0
+        sotart_ix = ts.find(piv.dt)
+        for ixx in range(start_ix, ts.pos + 1):
+            total_volume += ts.ix(ixx).volume * math.abs(ts.ix(ixx).c - ts.ix(ixx - 1).c)
