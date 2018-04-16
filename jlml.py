f0850f1075448586619568576994fe33fa03a7dd
golden-dawn <constantin_m_adam@yahoo.com>
2018-04-16 00:39:53 -0400

    WIP: generating the ML data, added analysis for each pivot.  Still need to cover case when two pivots happen  the same day

--- jlml.py
+++ jlml.py
@@ -6,9 +6,9 @@ from stxts import StxTS
 
 
 class JLML:
-    def __init__(self):
-        self.sd = '1950-01-01'
-        self.ed = '2020-12-31'
+    def __init__(self, sd='1950-01-01', ed='2020-12-31'):
+        self.sd = sd
+        self.ed = ed
         self.factors = [0.6, 1.1, 1.6]
 
     def gen_stk_data(self, stk):
@@ -27,24 +27,30 @@ class JLML:
                           ts.current_date(), ts.current('o'), ts.current('hi'),
                           ts.current('lo'), ts.current('c'),
                           ts.current('volume'), jl.avg_rg,
-                          self.print_stats(ts, pivs)))
+                          self.print_stats(jl, ts, pivs)))
 
     def print_stats(self, jl, ts, pivs):
         piv_data = []
         cc = ts.current('c')
+        ixx = 1
         for piv in reversed(pivs):
             num_days = stxcal.num_busdays(piv.dt, ts.current_date())
-            dist = (piv.price - cc) / (jl.avg_rg * math.sqrt(num_days))
+            dist = (cc - piv.price) / (jl.avg_rg * math.sqrt(num_days))
+            udv, udd = self.up_down_volume(ts, piv)
+            piv_data.append('P{0:d}: {1:s} {2:d} {3:.2f} {4:.2f} {5:.2f}'.
+                            format(ixx, piv.dt, num_days, dist, udv, udd))
+            ixx += 1
+        return '; '.join(piv_data)
 
     def up_down_volume(self, ts, piv):
         total_volume = 0
         signed_volume = 0
-        all_days = 0
-        signed_days = 0
-        sotart_ix = ts.find(piv.dt)
+        all_days = 0.0
+        signed_days = 0.0
+        start_ix = ts.find(piv.dt)
         for ixx in range(start_ix, ts.pos + 1):
-            total_volume += ts.ix(ixx).volume * math.abs(ts.ix(ixx).c -
-                                                         ts.ix(ixx - 1).c)
+            total_volume += ts.ix(ixx).volume * math.fabs(ts.ix(ixx).c -
+                                                          ts.ix(ixx - 1).c)
             signed_volume += ts.ix(ixx).volume * (ts.ix(ixx).c -
                                                   ts.ix(ixx - 1).c)
             all_days += 1
@@ -53,3 +59,9 @@ class JLML:
             elif ts.ix(ixx).c < ts.ix(ixx - 1).c:
                 signed_days -= 1
         return signed_volume / total_volume, signed_days / all_days
+
+
+if __name__ == '__main__':
+    stk = 'NFLX'
+    jlml = JLML('2002-05-20', '2005-09-30')
+    jlml.gen_stk_data(stk)
