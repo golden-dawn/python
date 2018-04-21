ab3a89f4203da19fdee7c4c2b49f2e3f5ba7579c
golden-dawn <constantin_m_adam@yahoo.com>
2018-04-21 02:22:09 -0400

    WIP: write the ML dataset to DB

--- jlml.py
+++ jlml.py
@@ -6,13 +6,71 @@ from stxts import StxTS
 
 
 class JLML:
+    sql_create_tbl = 'CREATE TABLE jlml ('\
+                     'stk varchar(16) NOT NULL,'\
+                     'dt date NOT NULL,'\
+                     'pl_3 decimal(2,1) DEFAULT NULL,'\
+                     'pl_5 decimal(2,1) DEFAULT NULL,'\
+                     'jl06p1_time float DEFAULT NULL,'\
+                     'jl06p1_dist float DEFAULT NULL,'\
+                     'jl06p1_udv float DEFAULT NULL,'\
+                     'jl06p1_udd float DEFAULT NULL,'\
+                     'jl06p2_time float DEFAULT NULL,'\
+                     'jl06p2_dist float DEFAULT NULL,'\
+                     'jl06p2_udv float DEFAULT NULL,'\
+                     'jl06p2_udd float DEFAULT NULL,'\
+                     'jl06p3_time float DEFAULT NULL,'\
+                     'jl06p3_dist float DEFAULT NULL,'\
+                     'jl06p3_udv float DEFAULT NULL,'\
+                     'jl06p3_udd float DEFAULT NULL,'\
+                     'jl06p4_time float DEFAULT NULL,'\
+                     'jl06p4_dist float DEFAULT NULL,'\
+                     'jl06p4_udv float DEFAULT NULL,'\
+                     'jl06p4_udd float DEFAULT NULL,'\
+                     'jl11p1_time float DEFAULT NULL,'\
+                     'jl11p1_dist float DEFAULT NULL,'\
+                     'jl11p1_udv float DEFAULT NULL,'\
+                     'jl11p1_udd float DEFAULT NULL,'\
+                     'jl11p2_time float DEFAULT NULL,'\
+                     'jl11p2_dist float DEFAULT NULL,'\
+                     'jl11p2_udv float DEFAULT NULL,'\
+                     'jl11p2_udd float DEFAULT NULL,'\
+                     'jl11p3_time float DEFAULT NULL,'\
+                     'jl11p3_dist float DEFAULT NULL,'\
+                     'jl11p3_udv float DEFAULT NULL,'\
+                     'jl11p3_udd float DEFAULT NULL,'\
+                     'jl11p4_time float DEFAULT NULL,'\
+                     'jl11p4_dist float DEFAULT NULL,'\
+                     'jl11p4_udv float DEFAULT NULL,'\
+                     'jl11p4_udd float DEFAULT NULL,'\
+                     'jl16p1_time float DEFAULT NULL,'\
+                     'jl16p1_dist float DEFAULT NULL,'\
+                     'jl16p1_udv float DEFAULT NULL,'\
+                     'jl16p1_udd float DEFAULT NULL,'\
+                     'jl16p2_time float DEFAULT NULL,'\
+                     'jl16p2_dist float DEFAULT NULL,'\
+                     'jl16p2_udv float DEFAULT NULL,'\
+                     'jl16p2_udd float DEFAULT NULL,'\
+                     'jl16p3_time float DEFAULT NULL,'\
+                     'jl16p3_dist float DEFAULT NULL,'\
+                     'jl16p3_udv float DEFAULT NULL,'\
+                     'jl16p3_udd float DEFAULT NULL,'\
+                     'jl16p4_time float DEFAULT NULL,'\
+                     'jl16p4_dist float DEFAULT NULL,'\
+                     'jl16p4_udv float DEFAULT NULL,'\
+                     'jl16p4_udd float DEFAULT NULL,'\
+                     'PRIMARY KEY (stk,dt)'\
+                     ')'
+
     def __init__(self, sd='1950-01-01', ed='2020-12-31'):
         self.sd = sd
         self.ed = ed
         self.factors = [0.6, 1.1, 1.6]
         self.time_adj = {0.6: 30.0, 1.1: 120.0, 1.6: 240.0}
+        # TODO: create table if missing
 
     def gen_stk_data(self, stk):
+        res = []
         ts = StxTS(stk, self.sd, self.ed)
         ts.df['c_3'] = ts.df['c'].shift(-3)
         ts.df['c_5'] = ts.df['c'].shift(-5)
@@ -30,32 +88,43 @@ class JLML:
             jl_lst.append(jl)
         for ixx in range(start_w, ts.end + 1):
             ts.next_day()
+            lst = [stk, ts.current_date(),
+                   (ts.current('c_3') - ts.current('c')) / jl.avg_rg,
+                   (ts.current('c_5') - ts.current('c')) / jl.avg_rg]
             for jl in jl_lst:
                 # jl = jl_lst[0]
                 jl.nextjl()
                 pivs = jl.get_num_pivots(4)
-                print('{0:.1f} {1:s} {2:.2f} {3:.2f} {4:.2f} rg: {5:.2f} '
-                      '{6:s}'.format(
-                          jl.f, ts.current_date(), ts.current('c'),
-                          (ts.current('c_3') - ts.current('c')) / jl.avg_rg,
-                          (ts.current('c_5') - ts.current('c')) / jl.avg_rg,
-                          jl.avg_rg, self.print_stats(jl, ts, pivs)))
+                lst += self.pivot_stats(jl, ts, pivs)
+                # print('{0:.1f} {1:s} {2:.2f} {3:.2f} {4:.2f} rg: {5:.2f} '
+                #       '{6:s}'.format(
+                #           jl.f, ts.current_date(), ts.current('c'),
+                #           (ts.current('c_3') - ts.current('c')) / jl.avg_rg,
+                #           (ts.current('c_5') - ts.current('c')) / jl.avg_rg,
+                #           jl.avg_rg, self.print_stats(jl, ts, pivs)))
+            res.append(lst)
+        with open('/tmp/jlml.txt', 'w') as f:
+            for r in res:
+                f.write('{0:s}\n'.format('\t'.join(r)))
+        # TODO upload file in database
 
-    def print_stats(self, jl, ts, pivs):
+    def pivot_stats(self, jl, ts, pivs):
         piv_data = []
         cc = ts.current('c')
-        ixx = 1
+        # ixx = 1
         for piv in reversed(pivs):
             num_days = stxcal.num_busdays(piv.dt, ts.current_date())
             if num_days == 0:
                 num_days = 0.5
             dist = (cc - piv.price) / (jl.avg_rg * math.sqrt(num_days))
             udv, udd = self.up_down_volume(ts, piv)
-            piv_data.append('P{0:d}: {1:s} {2:.2f} {3:.2f} {4:.2f} {5:.2f}'.
-                            format(ixx, piv.dt, num_days / self.time_adj[jl.f],
-                                   dist, udv, udd))
-            ixx += 1
-        return '; '.join(piv_data)
+            piv_data += [num_days / self.time_adj[jl.f], dist, udv, udd]
+            # piv_data.append('P{0:d}: {1:s} {2:.2f} {3:.2f} {4:.2f} {5:.2f}'.
+            #             format(ixx, piv.dt, num_days / self.time_adj[jl.f],
+            #                        dist, udv, udd))
+            # ixx += 1
+        return piv_data + [''] * (16 - len(piv_data))
+        # return '; '.join(piv_data)
 
     def up_down_volume(self, ts, piv):
         total_volume = 0
