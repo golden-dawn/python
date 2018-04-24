78385fcc6452e7995f4939bb388fe33a0c833a83
golden-dawn <constantin_m_adam@yahoo.com>
2018-04-24 01:33:19 -0400

    replace null pivots with zeros. Run with multiple stocks

--- jlml.py
+++ jlml.py
@@ -78,7 +78,7 @@ class JLML:
         ts.df['c3'] = ts.df['c'].shift(-3)
         ts.df['c5'] = ts.df['c'].shift(-5)
         ts.df['c_1'] = ts.df['c'].shift(1)
-        print(ts.df.tail(10))
+        # print(ts.df.tail(10))
         ts.df['udv'] = (ts.df['c'] - ts.df['c_1']) * ts.df['volume']
         ts.df['tot_v'] = np.abs(ts.df['udv'])
         ts.df['sgn'] = np.sign(ts.df['udv'])
@@ -105,12 +105,12 @@ class JLML:
                 pivs = jl.get_num_pivots(4)
                 # pivs = []
                 lst += self.pivot_stats(jl, ts, pivs)
-                print('{0:.1f} {1:s} {2:.2f} {3:.2f} {4:.2f} rg: {5:.2f} '
-                      'c3 = {6:.2f}, c5 = {7:.2f}'.format(
-                          jl.f, ts.current_date(), ts.current('c'),
-                          (ts.current('c3') - ts.current('c')) / jl.avg_rg,
-                          (ts.current('c5') - ts.current('c')) / jl.avg_rg,
-                          jl.avg_rg, ts.current('c3'), ts.current('c5')))
+                # print('{0:.1f} {1:s} {2:.2f} {3:.2f} {4:.2f} rg: {5:.2f} '
+                #       'c3 = {6:.2f}, c5 = {7:.2f}'.format(
+                #           jl.f, ts.current_date(), ts.current('c'),
+                #           (ts.current('c3') - ts.current('c')) / jl.avg_rg,
+                #           (ts.current('c5') - ts.current('c')) / jl.avg_rg,
+                #           jl.avg_rg, ts.current('c3'), ts.current('c5')))
                 # print('{0:.1f} {1:s} {2:.2f} {3:.2f} {4:.2f} rg: {5:.2f} '
                 #       '{6:s}'.format(
                 #           jl.f, ts.current_date(), ts.current('c'),
@@ -124,7 +124,8 @@ class JLML:
             for r in res[:-6]:
                 wrtr.writerow(r)
                 # f.write('{0:s}\n'.format('\t'.join([str(x) for x in r])))
-        # stxdb.db_upload_file(fname, 'ml')
+        stxdb.db_upload_file(fname, 'ml')
+        return len(res)
 
     def f(self, x, bound=3):
         if x <= -bound:
@@ -148,13 +149,14 @@ class JLML:
                 num_days = 0.5
             dist = (cc - piv.price) / (jl.avg_rg * math.sqrt(num_days))
             udv, udd = self.up_down_volume(ts, piv)
-            piv_data += [num_days / self.time_adj[jl.f], dist, udv, udd]
+            piv_data += [round(num_days / self.time_adj[jl.f], 3),
+                         round(dist, 3), round(udv, 3), round(udd, 3)]
             # piv_data += [num_days / self.time_adj[jl.f], dist, udv, udd]
             # piv_data.append('P{0:d}: {1:s} {2:.2f} {3:.2f} {4:.2f} {5:.2f}'.
             #             format(ixx, piv.dt, num_days / self.time_adj[jl.f],
             #                        dist, udv, udd))
             # ixx += 1
-        return piv_data + [None] * (16 - len(piv_data))
+        return piv_data + [0.0] * (16 - len(piv_data))
         # return '; '.join(piv_data)
 
     def up_down_volume(self, ts, piv):
@@ -170,10 +172,22 @@ class JLML:
 
 if __name__ == '__main__':
     stk = 'NFLX'
-    jlml = JLML('2002-05-20', '2002-08-30')
+    jlml = JLML()
+    # jlml = JLML('2002-05-20', '2018-08-30')
     t1 = datetime.datetime.now()
-    jlml.gen_stk_data(stk)
+    num = jlml.gen_stk_data(stk)
     t2 = datetime.datetime.now()
     delta = t2 - t1
-    print('Execution time: {0:f} seconds'.format(
-        (t2 - t1).seconds + (t2 - t1).microseconds / 1000000.0))
+    print('{0:s}: {1:d} records. Execution time: {2:f} seconds'.format(
+        stk, num, (t2 - t1).seconds + (t2 - t1).microseconds / 1000000.0))
+    r_stx = stxdb.db_read_cmd(
+        "select ticker from equities where ticker like 'R%'")
+    for r_stk in r_stx:
+        stk = r_stk[0]
+        print(stk)
+        t1 = datetime.datetime.now()
+        num = jlml.gen_stk_data(stk)
+        t2 = datetime.datetime.now()
+        delta = t2 - t1
+        print('{0:s}: {1:d} records. Execution time: {2:f} seconds'.format(
+            stk, num, (t2 - t1).seconds + (t2 - t1).microseconds / 1000000.0))
