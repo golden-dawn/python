611bf5092da3fb15ab495aa4bbc54a88a6e54317
golden-dawn <constantin_m_adam@yahoo.com>
2018-05-10 02:36:12 -0400

    First version of k-neighbors

--- stx_knn.py
+++ stx_knn.py
@@ -1,29 +1,34 @@
 import math
+import numpy as np
 import operator
 from stx_ml_loader import StxMlLoader
 
-def euclideanDistance(instance1, instance2, length):
-    distance = 0
-    for x in range(length):
-	distance += pow((instance1[x] - instance2[x]), 2)
-    return math.sqrt(distance)
 
-def getNeighbors(trainingSet, testInstance, k):
+def euclideanDistance(instance1, instance2, length):
+    return np.linalg.norm(np.array(instance1[4:]) - np.array(instance2[4:]))
+    # distance = 0
+    # for x in range(4, length):
+    #     distance += pow((instance1[x] - instance2[x]), 2)
+    # return math.sqrt(distance)
+
+
+def getNeighbors(train, testInstance, k):
     distances = []
     length = len(testInstance)-1
-    for x in range(len(trainingSet)):
-	dist = euclideanDistance(testInstance, trainingSet[x], length)
-	distances.append((trainingSet[x], dist))
-	distances.sort(key=operator.itemgetter(1))
-	neighbors = []
-	for x in range(k):
-	    neighbors.append(distances[x][0])
-	return neighbors
+    for x in range(len(train)):
+	dist = euclideanDistance(testInstance, train[x], length)
+	distances.append((train[x], dist))
+    distances.sort(key=operator.itemgetter(1))
+    neighbors = []
+    for x in range(k):
+	neighbors.append(distances[x][0])
+    return neighbors
 
-def getResponse(neighbors):
+
+def getResponse(neighbors, ixx):
     classVotes = {}
     for x in range(len(neighbors)):
-	response = neighbors[x][-1]
+	response = neighbors[x][ixx]
 	if response in classVotes:
 	    classVotes[response] += 1
 	else:
@@ -32,27 +37,43 @@ def getResponse(neighbors):
                          reverse=True)
     return sortedVotes[0][0]
 
- 
-def getAccuracy(testSet, predictions):
-	correct = 0
-	for x in range(len(testSet)):
-		if testSet[x][-1] == predictions[x]:
-			correct += 1
-	return (correct/float(len(testSet))) * 100.0
-	
+
+def getAccuracy(test, predictions, ixx):
+    correct = 0
+    for x in range(len(test)):
+	if test[x][ixx] == predictions[x]:
+	    correct += 1
+    return (correct / float(len(test))) * 100.0
+
+
 def main():
     sml = StxMlLoader()
-    sml = StxMlLoader()
-    train, validation, test = sml.get_data()
+    sml = StxMlLoader(train_q="select * from ml where stk like 'R%' and dt between '2001-12-15' and '2002-02-08'", tst_q="select * from ml where stk like 'R%' and dt between '2002-02-08' and '2002-02-28'")
+    train, validation, test = sml.get_raw_data()
     # generate predictions
     predictions=[]
-    k = 3
+    k = 15
+    ixx = 3
+    print('Predicted, Actual')
+    n = 0
+    m = len(test)
+    res = {}
     for x in range(len(test)):
 	neighbors = getNeighbors(train, test[x], k)
-	result = getResponse(neighbors)
+	result = getResponse(neighbors, ixx)
 	predictions.append(result)
-	print('> predicted = {0:s}' + repr(result) + ', actual=' + repr(testSet[x][-1]))
-    accuracy = getAccuracy(testSet, predictions)
+        n += 1
+        if n % 100 == 0 or n == m:
+            print('Processed {0:5d} out of {1:5d}'.format(n, m))
+        actual = test[x][ixx]
+        row = res.get(actual, [0, 0, 0])
+        row[result + 1] += 1
+        res[actual] = row
+    for k, v in res.items():
+        v_sum = float(np.sum(v))
+        print('{0:d}:  {1:4.2f}  {2:4.2f}  {3:4.2f}'.
+              format(k, v[0] / v_sum, v[1] / v_sum, v[2] / v_sum))
+    accuracy = getAccuracy(test, predictions, ixx)
     print('Accuracy: ' + repr(accuracy) + '%')
 	
 main()
