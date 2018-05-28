ab9cb4a2ddc83a6d3cc93aa094ce2ee0244337ed
golden-dawn <constantin_m_adam@yahoo.com>
2018-05-28 01:26:13 -0400

    Further exposition of the internal calculations and experiment with different selection methodologies

--- stx_knn.py
+++ stx_knn.py
@@ -4,6 +4,7 @@ from stx_ml_loader import StxMlLoader
 import sys
 import stxcal
 
+
 def euclideanDistance(instance1, instance2, length):
     return np.linalg.norm(np.array(instance1[4:]) - np.array(instance2[4:]))
     # distance = 0
@@ -12,24 +13,22 @@ def euclideanDistance(instance1, instance2, length):
     # return math.sqrt(distance)
 
 
-def getNeighbors(train, testInstance, k):
+def getNeighbors(train, testInstance, k, thresh):
     distances = []
     length = len(testInstance)-1
     stk = testInstance[0]
     dt = str(testInstance[1])
     for x in range(len(train)):
         if stk == train[x][0] and stxcal.num_busdays(
-            str(train[x][1]), dt) < 180:
+                str(train[x][1]), dt) < 180:
             continue
         dist = euclideanDistance(testInstance, train[x], length)
         distances.append((train[x], dist))
     distances.sort(key=operator.itemgetter(1))
     neighbors = []
     n_dict = {}
-    m = 0
-    d = 0
-    x = 0
-    while m < k and d < 0.75:
+    m, d, x = 0, 0, 0
+    while m < k and d < thresh:
         n_stk = distances[x][0][0]
         n_date = str(distances[x][0][1])
         d = distances[x][1]
@@ -37,14 +36,15 @@ def getNeighbors(train, testInstance, k):
         if n_stk in n_dict:
             n_add = False
         else:
-            if d < 1.25:
+            if d < thresh:
                 n_dict[n_stk] = n_date
             else:
                 n_add = False
-        print('{0:s} {1:s} [{2:d}] Neighbor{3:d}: {4:s} - {5:s}, [{6:d}] '
-              'distance = {7:.2f}, add = {8:s}'.format(
-                  stk, dt, testInstance[3], x, n_stk, n_date,
-                  distances[x][0][3], d, str(n_add)))
+        if n_add:
+            print('{0:s} {1:s} [{2:d}] Neighbor{3:d}: {4:s} - {5:s}, [{6:d}] '
+                  'distance = {7:.2f}, add = {8:s}'.format(
+                      stk, dt, testInstance[3], x, n_stk, n_date,
+                      distances[x][0][3], d, str(n_add)))
         if n_add:
             neighbors.append(distances[x][0])
             m += 1
@@ -52,7 +52,7 @@ def getNeighbors(train, testInstance, k):
     return neighbors
 
 
-def getResponse(neighbors, ixx):
+def getResponse(neighbors, ixx, k):
     classVotes = {}
     for x in range(len(neighbors)):
         response = neighbors[x][ixx]
@@ -62,9 +62,11 @@ def getResponse(neighbors, ixx):
             classVotes[response] = 1
     sortedVotes = sorted(classVotes.iteritems(), key=operator.itemgetter(1),
                          reverse=True)
+    print(sortedVotes)
     if len(sortedVotes) > 0:
-        return sortedVotes[0][0]
-    return None
+        if sortedVotes[0][1] > k / 2:
+            return sortedVotes[0][0]
+    return 0
 
 
 def getAccuracy(test, predictions, ixx):
@@ -82,6 +84,7 @@ def main():
     tst_ed = sys.argv[4]
     ixx = int(sys.argv[5])
     k = int(sys.argv[6])
+    thresh = float(sys.argv[7])
     sml = StxMlLoader()
 
     train_q = "select stk, dt, pl_3, pl_5, jl11p1_time, jl11p1_dist, "\
@@ -96,7 +99,7 @@ def main():
         "jl11p4_dist, jl11p4_udv, jl11p4_udd, jl16p1_time, jl16p1_dist, "\
         "jl16p2_time, jl16p2_dist, jl16p2_udv, jl16p2_udd, jl16p3_time, "\
         "jl16p3_dist, jl16p4_udv, jl16p4_udd, jl16p4_time, jl16p4_dist "\
-        "from ml where stk like 'R%' and dt between '{0:s}' and '{1:s}'".\
+        "from ml where stk='RIG' and dt between '{0:s}' and '{1:s}'".\
         format(tst_sd, tst_ed)
 
     # train_q = "select * from ml where stk like 'R%' and dt between "\
@@ -113,8 +116,8 @@ def main():
     m = len(test)
     res = {}
     for x in range(len(test)):
-        neighbors = getNeighbors(train, test[x], k)
-        result = getResponse(neighbors, ixx)
+        neighbors = getNeighbors(train, test[x], k, thresh)
+        result = getResponse(neighbors, ixx, k)
         if result is not None:
             predictions.append(result)
             actual = test[x][ixx]
