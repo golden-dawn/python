909197ff0fb77c2eba89a5646c02132e876db1b8
golden-dawn <constantin_m_adam@yahoo.com>
2018-07-20 00:43:16 -0400

    WIP: candle setups

--- stx_candles.py
+++ stx_candles.py
@@ -8,6 +8,15 @@ class StxCandles:
         self.stk_name = stk_name
         self.avg_volume_days = avg_volume_days
         self.avg_range_days = avg_range_days
+        self.marubozu_ratio = 0.8
+        self.long_day_avg_ratio = 1.3
+        self.short_day_avg_ratio = 0.5
+        self.engulfing_ratio = 0.7
+        self.harami_ratio = 0.3
+        self.umbrella_body_lower_shadow_ratio = 0.33
+        self.umbrella_upper_shadow_range_ratio = 0.1
+        self.doji_body_range_ratio = 0.025
+        self.equal_values_range_ratio = 0.025
 
     def calculate_setups(self):
         sd = '1985-01-01'
@@ -18,3 +27,4 @@ class StxCandles:
         ts.df['body'] = abs(ts.df['o'] - ts.df['c'])
         ts.df['avg_body'] = ts.df['body'].rolling(self.avg_range_days)
         ts.df['avg_v'] = ts.df['volume'].rolling(self.avg_volume_days)
+        
