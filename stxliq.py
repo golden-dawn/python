a1de04716bb5713d9fdc0ca42e921cfcbb92a74d
golden-dawn <constantin_m_adam@yahoo.com>
2018-11-28 00:17:48 -0500

    Added function to find all the liquidity leaders as of a specific date

--- stxliq.py
+++ stxliq.py
@@ -1,6 +1,10 @@
 import numpy as np
+import pandas as pd
+import stxcal
 import stxdb
 from stxts import StxTS
+from stx_candles import StxCandles
+import sys
 
 
 class StxLiquidity:
@@ -43,7 +47,57 @@ class StxLiquidity:
             return ts
         return None
 
+    def find_all_liquid_stocks_as_of(self, selected_date):
+        res = []
+        q = "select * from eods where date = '{0:s}'".format(selected_date)
+        df = pd.read_sql(q, stxdb.db_get_cnx())
+        print('Found {0:d} stocks'.format(len(df)))
+        df['rg'] = df['hi'] - df['lo']
+        df_1 = df.query('volume>1000000 & c>30 & rg>0.015*c')
+        stx = df_1['stk'].tolist()
+        print('Found {0:d} leaders'.format(len(stx)))
+        start_date = stxcal.move_busdays(selected_date, -60)
+        ixx = 0
+        for stk in stx:
+            ixx += 1
+            ts = StxTS(stk, start_date, selected_date)
+            # adjust the whole thing for splits, etc.
+            ts.set_day(str(ts.df.index[-1].date()))
+            ts.df['hi_1'] = ts.df['hi'].shift(1)
+            ts.df['lo_1'] = ts.df['lo'].shift(1)
+            ts.df['rg'] = ts.df['hi'] - ts.df['lo']
+            ts.df['act'] = ts.df['volume'] * ts.df['c']
+            ts.df['avg_v'] = ts.df['volume'].rolling(50).mean()
+            ts.df['avg_c'] = ts.df['c'].rolling(50).mean()
+            ts.df['avg_rg'] = ts.df['rg'].rolling(50).mean()
+            ts.df['avg_act'] = ts.df['act'].rolling(50).mean()
+            rec = ts.df.ix[-1]
+            if rec.avg_v > 2000000 and rec.avg_c > 40 and \
+               rec.avg_act > 100000000 and rec.avg_rg > 0.015 * rec.avg_c:
+                res.append(stk)
+                sc = StxCandles(stk)
+                setup_ts = sc.calculate_setups(sd=start_date)
+                setups = ['gap', 'marubozu', 'hammer', 'doji', 'engulfing',
+                          'piercing', 'harami', 'star', 'engulfharami',
+                          'three_m', 'three_in', 'three_out',
+                          'up_gap_two_crows']
+                with open('/home/cma/setups/{0:s}.csv'.format(stk), 'w') as f:
+                    for index, row in setup_ts.df.iterrows():
+                        f.write('{0:s};'.format(str(index.date())))
+                        for setup in setups:
+                            if row[setup] != 0:
+                                f.write('  {0:s}: {1:.0f} '.format(
+                                    setup.upper(), row[setup]))
+                        f.write('\n')
+            if ixx == len(stx) or ixx % 50 == 0:
+                print('Processed {0:d} leaders'.format(ixx))
+        print('Found {0:d} super leaders'.format(len(res)))
+        return res
+
 
 if __name__ == '__main__':
     liq = StxLiquidity()
-    liq.find_all_liquid_stocks()
+    selected_date = sys.argv[1]
+    stx = liq.find_all_liquid_stocks_as_of(selected_date)
+    print(stx)
+    # liq.find_all_liquid_stocks()
