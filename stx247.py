b1a34f776667efcf36f5e2c7b363618337a70091
golden-dawn <constantin_m_adam@yahoo.com>
2019-02-15 22:28:56 -0500

    Create cache tables for eod and options and schedule cron job for analysis

--- stx247.py
+++ stx247.py
@@ -1,28 +1,8 @@
-require 'schedule'
+import schedule
+import stxdb
+import time
 
-class StxTFS:
-    self.sql_create_eod_tbl = "CREATE TABLE {0:s} ("\
-                              "stk varchar(8) NOT NULL,"\
-                              "dt date NOT NULL,"\
-                              "o numeric(10,2) DEFAULT NULL,"\
-                              "hi numeric(10,2) DEFAULT NULL,"\
-                              "lo numeric(10,2) DEFAULT NULL,"\
-                              "c numeric(10,2) DEFAULT NULL,"\
-                              "v integer DEFAULT NULL,"\
-                              "PRIMARY KEY (stk,dt)"\
-                              ")".format(self.tbl_name)
-    self.sql_create_opt_tbl = "CREATE TABLE {0:s} ("\
-                              'expiry date NOT NULL,'\
-                              'und character varying(16) NOT NULL,'\
-                              'cp character varying(1) NOT NULL,'\
-                              'strike numeric(10,2) NOT NULL,'\
-                              'dt date NOT NULL,'\
-                              'bid numeric(10,2),'\
-                              'ask numeric(10,2),'\
-                              'volume integer,'\
-                              'PRIMARY KEY (expiry,und,cp,strike,dt)'\
-                              ')'.format(self.tbl_name)
-    
+class Stx247:    
     '''
 1. In the constructor, create the database tables, if non-existent
 2. Schedule two runs, one at 15:30, the other at 20:00
@@ -31,3 +11,42 @@ class StxTFS:
 5. Have exclusion list, to remove stocks I don't need.
     6.   
     '''
+    def __init__(self):
+        self.tbl_name = 'cache'
+        self.opt_tbl_name = 'opt_cache'
+        self.sql_create_eod_tbl = "CREATE TABLE {0:s} ("\
+                                  "stk varchar(8) NOT NULL,"\
+                                  "dt date NOT NULL,"\
+                                  "o numeric(10,2) DEFAULT NULL,"\
+                                  "hi numeric(10,2) DEFAULT NULL,"\
+                                  "lo numeric(10,2) DEFAULT NULL,"\
+                                  "c numeric(10,2) DEFAULT NULL,"\
+                                  "v integer DEFAULT NULL,"\
+                                  "PRIMARY KEY (stk,dt)"\
+                                  ")".format(self.tbl_name)
+        self.sql_create_opt_tbl = "CREATE TABLE {0:s} ("\
+                                  'expiry date NOT NULL,'\
+                                  'und character varying(16) NOT NULL,'\
+                                  'cp character varying(1) NOT NULL,'\
+                                  'strike numeric(10,2) NOT NULL,'\
+                                  'dt date NOT NULL,'\
+                                  'bid numeric(10,2),'\
+                                  'ask numeric(10,2),'\
+                                  'volume integer,'\
+                                  'PRIMARY KEY (expiry,und,cp,strike,dt)'\
+                                  ')'.format(self.opt_tbl_name)
+        stxdb.db_create_missing_table(self.tbl_name, self.sql_create_eod_tbl)
+        stxdb.db_create_missing_table(self.opt_tbl_name,
+                                      self.sql_create_opt_tbl)
+
+    def job(self):
+        print('247 job')
+
+
+if __name__ == '__main__':
+    s247= Stx247()
+    schedule.every(10).seconds.do(s247.job)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
