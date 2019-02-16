60353aea78f95fc0ec2f10e08fddf03280f1d986
golden-dawn <constantin_m_adam@yahoo.com>
2019-02-15 23:01:59 -0500

    Create tables for leaders and setups

--- stx247.py
+++ stx247.py
@@ -3,17 +3,20 @@ import stxdb
 import time
 
 class Stx247:    
-    '''
-1. In the constructor, create the database tables, if non-existent
-2. Schedule two runs, one at 15:30, the other at 20:00
-3. Email the analysis results: https://medium.freecodecamp.org/send-emails-using-code-4fcea9df63f
-4. If the current date is an option expiry, the 20:00 run should be generating new list of leaders.
-5. Have exclusion list, to remove stocks I don't need.
-    6.   
+    '''1. In the constructor, create the database tables, if non-existent
+    2. Schedule two runs, one at 15:30, the other at 20:00
+    3. Email the analysis results:
+       https://medium.freecodecamp.org/send-emails-using-code-4fcea9df63f
+    4. If the current date is an option expiry, the 20:00 run should
+       be generating new list of leaders.
+    5. Have exclusion list, to remove stocks I don't need.
+    6. 
     '''
     def __init__(self):
         self.tbl_name = 'cache'
         self.opt_tbl_name = 'opt_cache'
+        self.ldr_tbl_name = 'leaders'
+        self.setup_tbl_name = 'setups'
         self.sql_create_eod_tbl = "CREATE TABLE {0:s} ("\
                                   "stk varchar(8) NOT NULL,"\
                                   "dt date NOT NULL,"\
@@ -35,9 +38,25 @@ class Stx247:
                                   'volume integer,'\
                                   'PRIMARY KEY (expiry,und,cp,strike,dt)'\
                                   ')'.format(self.opt_tbl_name)
+        self.sql_create_ldr_tbl = "CREATE TABLE {0:s} ("\
+                                  "stk varchar(8) NOT NULL,"\
+                                  "dt date NOT NULL,"\
+                                  "exp date NOT NULL,"\
+                                  "PRIMARY KEY (stk,dt)"\
+                                  ")".format(self.ldr_tbl_name)
+        self.sql_create_setup_tbl = "CREATE TABLE {0:s} ("\
+                                    "stk varchar(8) NOT NULL,"\
+                                    "dt date NOT NULL,"\
+                                    "setup varchar(80) NOT NULL,"\
+                                    "PRIMARY KEY (stk,dt)"\
+                                  ")".format(self.setup_tbl_name)
         stxdb.db_create_missing_table(self.tbl_name, self.sql_create_eod_tbl)
         stxdb.db_create_missing_table(self.opt_tbl_name,
                                       self.sql_create_opt_tbl)
+        stxdb.db_create_missing_table(self.ldr_tbl_name,
+                                      self.sql_create_ldr_tbl)
+        stxdb.db_create_missing_table(self.setup_tbl_name,
+                                      self.sql_create_setup_tbl)
 
     def job(self):
         print('247 job')
