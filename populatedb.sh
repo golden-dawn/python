52afd4f7f5aaa6c47497bbf0d77ef17a1ed6f9f2
golden-dawn <constantin_m_adam@yahoo.com>
2019-05-04 03:51:53 -0400

    Added status messages

--- populatedb.sh
+++ populatedb.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+echo -e "\n\n python stx_ng.py\n\n "
 python stx_ng.py
+echo -e "\n\n python opteod.py\n\n "
 python opteod.py
+echo -e "\n\n python stx_eod.py --stooq\n\n "
 python stx_eod.py --stooq
+echo -e "\n\n python stx_eod.py --batch\n\n "
 python stx_eod.py --batch
