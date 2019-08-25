9b2a496a6f9c61e2e69a0529928e4167e16a1ca5
golden-dawn <constantin_m_adam@yahoo.com>
2019-08-25 10:06:59 -0400

    Added EOD fixes to intraday analysis script

--- intraday.sh
+++ intraday.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 export CRT_DIR=${PWD}
+cd ${HOME}
+source .env
+source .bash_profile.pysave
 cd ${HOME}/c
-./stx_ana.exe --intraday
+./stx_ana.exe -intraday
 cd ${HOME}/python
+source .env
 python stx_mail.py --intraday
 cd ${CRT_DIR}
