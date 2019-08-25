8970f9b2b8c9120f2c6130b6adc0e2cd2132f1a7
golden-dawn <constantin_m_adam@yahoo.com>
2019-08-25 10:03:44 -0400

    Fixed analysis to run from cronjob; still fails to email the results

--- eod.sh
+++ eod.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 export CRT_DIR=${PWD}
+cd ${HOME}
+source .env
+source .bash_profile.pysave
 cd ${HOME}/c
-./stx_ana.exe --eod
+./stx_ana.exe -eod
 cd ${HOME}/python
+source .env
 python stx_mail.py --eod
 cd ${CRT_DIR}
