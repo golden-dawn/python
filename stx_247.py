734657b9043c11fa5e604065b4f6cd8e0252fdcb
golden-dawn <constantin_m_adam@yahoo.com>
2019-10-09 02:04:18 -0400

    Incorporate gap/strong close/volume study

--- stx_247.py
+++ stx_247.py
@@ -30,14 +30,39 @@ class StxAnalyzer:
 
     def do_analysis(self, crt_date, max_spread):
         exp_date = stxcal.next_expiry(crt_date)
+        five_days_ago = stxcal.move_busdays(crt_date, -5)
         res = '{0:6s} {1:9s} {2:6s} {3:12s} {4:6s}'.format(
             'name', 'direction', 'spread', 'avg_volume', 'avg_rg')
         #      dt     |  stk  |  setup   | direction | triggered 
         # ------------+-------+----------+-----------+-----------
         #  2019-08-21 | MSM   | JC_1234  | D         | t
         q1 = sql.Composed([sql.SQL('select * from setups where dt='),
-                          sql.Literal(crt_date)])
+                           sql.Literal(crt_date), sql.SQL('and setup in ('),
+                           sql.Literal('JC_5DAYS'), sql.SQL(','), 
+                           sql.Literal('JC_1234'), sql.SQL(')')])
         df = pd.read_sql(q1, stxdb.db_get_cnx())
+        df['d_5'] = five_days_ago
+
+        def hiactfun(r):
+            qha = sql.Composed(
+                [sql.SQL('select * from setups where dt between '),
+                 sql.Literal(r['d_5']), 
+                 sql.SQL(' and '),
+                 sql.Literal(r['dt']),
+                 sql.SQL(' and stk='),
+                 sql.Literal(r['stk']),
+                 sql.SQL(' and setup in ('),
+                 sql.SQL(',').join([sql.Literal('GAP_HV'), 
+                                    sql.Literal('STRONG_CLOSE')]),
+                 sql.SQL(')')])
+            cnx = stxdb.db_get_cnx()
+            rows = []
+            with cnx.cursor() as crs:
+                crs.execute(qha.as_string(cnx))
+                rows = crs.fetchall()
+            return len(rows) if rows else 0
+        df['ha'] = df.apply(hiactfun, axis=1)
+
         q2 = sql.Composed([sql.SQL('select stk, opt_spread from leaders '
                                    'where expiry='), sql.Literal(exp_date)])
         cnx = stxdb.db_get_cnx()
@@ -58,9 +83,10 @@ class StxAnalyzer:
                                         ts.df['lo'].values[-20:], 
                                         ts.df['c'].values[-21:-1])]
             avg_rg = np.average(rgs)
-            res = '{0:s}\r\n{1:6s} {2:9s} {3:6d} {4:12,d} {5:6.2f}'.format(
-                    res, stk, row['direction'], row['spread'],
-                    int(1000 * avg_volume), avg_rg / 100)
+
+            res = '{0:s}\r\n{1:6s} {2:9s} {3:6d} {4:12,d} {5:6.2f} '\
+                '{6:d}'.format(res, stk, row['direction'], row['spread'],
+                               int(1000 * avg_volume), avg_rg / 100, row['ha'])
         print('{0:s}:'.format(crt_date))
         print('===========')
         print(res)
