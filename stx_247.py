e9fb2d135c401465d4ad15a461eb6f3f052a1a55
golden-dawn <constantin_m_adam@yahoo.com>
2019-10-15 02:57:17 -0400

    WIP: add triggered and tomorrow's setups

--- stx_247.py
+++ stx_247.py
@@ -18,31 +18,29 @@ import traceback
 
 class StxAnalyzer:
 
-    def __init__(self):
-        pass
+    def get_triggered_setups(self, dt):
+        q = sql.Composed([
+                sql.SQL('select * from setups where dt='), sql.Literal(dt), 
+                sql.SQL(' and setup in ('), sql.SQL(', ').join(
+                    [sql.Literal('JC_5DAYS'), sql.Literal('JC_1234')]), 
+                sql.SQL(') and triggered='), sql.Literal(True)])
+        df = pd.read_sql(q, stxdb.db_get_cnx())
+        return df
 
-#         for stk in stx:
-#             ts = StxTS(stk, self.start_date, ana_date)
-#             for ixx in range(1, 5):
-#                 ts.df['hi_{0:d}'.format(ixx)] = ts.df['hi'].shift(ixx)
-#                 ts.df['lo_{0:d}'.format(ixx)] = ts.df['lo'].shift(ixx)
-#             ts.df['v_50'] = ts.df['volume'].rolling(50).mean()
+    def get_setups_for_tomorrow(self, dt):
+        next_dt = stxcal.next_busday(dt)
+        q = sql.Composed([
+                sql.SQL('select * from setups where dt='), 
+                sql.Literal(next_dt), sql.SQL(' and setup in ('), 
+                sql.SQL(', ').join(
+                    [sql.Literal('JC_5DAYS'), sql.Literal('JC_1234')]), 
+                sql.SQL(')')])
+        df = pd.read_sql(q, stxdb.db_get_cnx())
+        return df
 
-    def do_analysis(self, crt_date, max_spread):
-        exp_date = stxcal.next_expiry(crt_date)
-        five_days_ago = stxcal.move_busdays(crt_date, -5)
-        res = '{0:6s} {1:9s} {2:6s} {3:12s} {4:6s}'.format(
-            'name', 'direction', 'spread', 'avg_volume', 'avg_rg')
-        #      dt     |  stk  |  setup   | direction | triggered 
-        # ------------+-------+----------+-----------+-----------
-        #  2019-08-21 | MSM   | JC_1234  | D         | t
-        q1 = sql.Composed([sql.SQL('select * from setups where dt='),
-                           sql.Literal(crt_date), sql.SQL('and setup in ('),
-                           sql.Literal('JC_5DAYS'), sql.SQL(','), 
-                           sql.Literal('JC_1234'), sql.SQL(')')])
-        df = pd.read_sql(q1, stxdb.db_get_cnx())
+    def get_high_activity(self, dt, df):
+        five_days_ago = stxcal.move_busdays(dt, -5)
         df['d_5'] = five_days_ago
-
         def hiactfun(r):
             qha = sql.Composed(
                 [sql.SQL('select * from setups where dt between '),
@@ -63,6 +61,20 @@ class StxAnalyzer:
             return len(rows) if rows else 0
         df['ha'] = df.apply(hiactfun, axis=1)
 
+
+    def do_analysis(self, crt_date, max_spread, eod):
+        res = '{0:6s} {1:9s} {2:6s} {3:12s} {4:6s} {5:6s}'.format(
+            'name', 'direction', 'spread', 'avg_volume', 'avg_rg', 'hi_act')
+        df_1 = self.get_triggered_setups(crt_date)
+        self.get_high_activity(crt_date, df_1)
+        if eod:
+            df_2 = self.get_setups_for_tomorrow(crt_date)
+            self.get_high_activity(crt_date, df_2)
+        #      dt     |  stk  |  setup   | direction | triggered 
+        # ------------+-------+----------+-----------+-----------
+        #  2019-08-21 | MSM   | JC_1234  | D         | t
+
+        exp_date = stxcal.next_expiry(crt_date)
         q2 = sql.Composed([sql.SQL('select stk, opt_spread from leaders '
                                    'where expiry='), sql.Literal(exp_date)])
         cnx = stxdb.db_get_cnx()
@@ -77,7 +89,7 @@ class StxAnalyzer:
         for _, row in df.iterrows():
             stk = row['stk']
             ts = StxTS(stk, s_date, crt_date)
-            avg_volume = np.average(ts.df['v'].values[:])
+            avg_volume = np.average(ts.df['v'].values[-20:])
             rgs = [max(h, c_1) - min(l, c_1) 
                    for h, l, c_1 in zip(ts.df['hi'].values[-20:], 
                                         ts.df['lo'].values[-20:], 
@@ -90,7 +102,30 @@ class StxAnalyzer:
         print('{0:s}:'.format(crt_date))
         print('===========')
         print(res)
-        
+
+    def mail_analysis(self, analysis_results, analysis_type):
+        smtp_server = os.getenv('EMAIL_SERVER')
+        smtp_user = os.getenv('EMAIL_USER')
+        smtp_passwd = os.getenv('EMAIL_PASSWD')
+        smtp_email = os.getenv('EMAIL_USER')
+        smtp_port = os.getenv('EMAIL_PORT')
+        try:
+            try:
+                s = smtplib.SMTP(host=smtp_server, port=smtp_port)
+                s.starttls()
+                s.login(smtp_user, smtp_passwd)
+                msg = MIMEText(analysis_results, 'plain')
+                msg['Subject'] = '{0:s} {1:s}'.format(analysis_type, crt_date)
+                msg['From'] = smtp_email
+                msg['To'] = smtp_email
+                s.sendmail(smtp_email, smtp_email, msg.as_string())
+            except:
+                print('Something failed: {0:s}'.format(traceback.print_exc()))
+            finally:
+                s.quit()
+        except:
+            print('Failed to send email: {0:s}'.format(traceback.print_exc()))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -99,6 +134,20 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--date', type=str, 
                         default=stxcal.current_busdate(),
                         help='Date to retrieve setups')
+    parser.add_argument('-e', '--eod', action='store_true',
+                        help="Run EOD analysis")
+    parser.add_argument('-i', '--intraday', action='store_true',
+                        help="Run Intraday analysis")    
+    parser.add_argument('-m', '--mail', action='store_true',
+                        help="Email analysis results")    
     args = parser.parse_args()
+    analysis_type = 'Analysis'
+    if args.eod:
+        analysis_type = 'EOD'
+    if args.intraday:
+        analysis_type = 'Intraday'
     stx_ana = StxAnalyzer()
-    stx_ana.do_analysis(args.date, args.max_spread)
+    analysis_results = stx_ana.do_analysis(args.date, args.max_spread, 
+                                           analysis_type)
+    if args.mail:
+        stx_ana.mail_analysis(analysis_results, analysis_type)
