6b769d5c768e2327fa4a6cd9a55b30c94205893a
golden-dawn <constantin_m_adam@yahoo.com>
2019-10-26 23:43:21 -0400

    script to backup database

--- backup_db.sh
+++ backup_db.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 export CRT_DIR=$(pwd)
-echo "psql -d stx -f backup_db.sql -v last_dt=$2 -v exp1=$3 -v exp2=$4 -v exp3=$5 -v exp4=$6"
-psql -d stx -f backup_db.sql -v last_dt=$2 -v exp1=$3 -v exp2=$4 -v exp3=$5 -v exp4=$6
+export VOLUME_NAME=${1}
+export LAST_DATE=${2}
+export EXP1=${3}
+export EXP2=${4}
+export EXP3=${5}
+export EXP4=${6}
+sed -e "s/LAST_DT/${LAST_DATE}/g" -e "s/EXP1/${EXP1}/g" -e "s/EXP2/${EXP2}/g" -e "s/EXP3/${EXP3}/g" -e "s/EXP4/${EXP4}/g" backup_db.sql > /tmp/backup_db.sql
+echo "psql -d stx -f /tmp/backup_db.sql"
+psql -d stx -f /tmp/backup_db.sql
 cd /tmp
 tar cvf db_bkp.tar eods.csv divis.csv ldrs.csv setups.csv opts.csv
 gzip db_bkp.tar
-cp db_bkp.tar.gz /Volumes/$1/
+cp db_bkp.tar.gz /Volumes/${VOLUME_NAME}/
