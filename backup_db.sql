7e9d3b38382497c09bdeddcd43a0770f7fc6bf9c
golden-dawn <constantin_m_adam@yahoo.com>
2019-10-26 23:43:13 -0400

    script to backup database

--- backup_db.sql
+++ backup_db.sql
@@ -1,6 +1,6 @@
-\copy (select * from eods where dt > :last_dt) to /tmp/eods.csv with CSV;
-\copy (select * from dividends where dt>:'last_dt') to /tmp/divis.csv with CSV;
-\copy (select * from options where expiry in (:'exp1', :'exp2', :'exp3', :'exp4') and  dt>:'last_dt') to /tmp/opts.csv with CSV;
+\copy (select * from eods where dt > 'LAST_DT') to /tmp/eods.csv with CSV;
+\copy (select * from dividends where dt > 'LAST_DT') to /tmp/divis.csv with CSV;
+\copy (select * from options where expiry in ('EXP1', 'EXP2', 'EXP3', 'EXP4') and  dt>'LAST_DT') to /tmp/opts.csv with CSV;
 \copy (select * from leaders) to /tmp/ldrs.csv with CSV;
 \copy (select * from setups) to /tmp/setups.csv with CSV;
 
