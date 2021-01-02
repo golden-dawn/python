8ae5d0f6485abf5385d229f39d5d0c004cd3282f
golden-dawn <constantin_m_adam@yahoo.com>
2021-01-02 02:57:13 -0500

    Clean up the code

--- stx_datafeed.py
+++ stx_datafeed.py
@@ -49,14 +49,6 @@ class StxDatafeed:
             'sq': 'stooq'
         }.get(x, 'final')
 
-    # def create_exchange(self):
-    #     xchgs = stxdb.db_read_cmd("select * from exchanges where name='US'")
-    #     if not xchgs:
-    #         stxdb.db_write_cmd("insert into exchanges values('US')")
-    #  db_stx = {x[0]: 0 for x in stxdb.db_read_cmd("select * from equities")}
-    #     stx = {}
-    #     return db_stx, stx
-
     # Load data from EODData data source in the database. There is a
     # daily file for each exchange (Amex, Nasdaq, NYSE). Use an
     # overlap of 5 days with the previous reconciliation interval
@@ -133,86 +125,6 @@ class StxDatafeed:
         else:
             stxdb.db_insert_eods(upload_lines)
 
-    def parseeodline(self, line):
-        stk, _, dt, o, h, l, c, v, oi = line.split(',')
-        # look only at the US stocks, for the time being
-        if not stk.endswith('.US'):
-            return
-        dt = '{0:s}-{1:s}-{2:s}'.format(dt[0:4], dt[4:6], dt[6:8])
-        if not stxcal.is_busday(dt):
-            raise Exception('{0:s} is not a business day'.format(dt))
-        o = int(100 * float(o))
-        h = int(100 * float(h))
-        l = int(100 * float(l))
-        c = int(100 * float(c))
-        # Make sure o and c are in the interval [l, h]
-        o = o if o <= h and o >= l else (h if o > h else l)
-        c = c if c <= h and c >= l else (h if c > h else l)
-        if o >= 2147483647 or h >= 2147483647 or l >= 2147483647 or \
-           c >= 2147483647:
-            return
-        v, oi = int(v), int(oi)
-        if stk.endswith('.US'):  # proces stock tickers, volume must be > 0
-            stk = stk[:-3].replace("-.", ".P.").replace("_", ".").replace(
-                '-', '.')
-            if v == 0:
-                raise Exception('Zero volume for stock')
-            if len(stk) > 8:
-                raise Exception('Ticker {0:s} too long'.format(stk))
-            v = v // 1000
-            if v == 0:
-                v = 1
-        elif stk.endswith('.B'):  # multiply bond prices by 10000
-            o, h, l, c = self.multiply_prices(o, h, l, c, 10000)
-        elif stk.endswith('6.F'):  # multiply currency future prices by 10000
-            o, h, l, c = self.multiply_prices(o, h, l, c, 10000)
-        elif stk in ['HO.F', 'NG.F', 'RB.F']:  # express prices in cents
-            o, h, l, c = self.multiply_prices(o, h, l, c, 100)
-        elif stk.startswith('^'):  # divide index volumes by 1000
-            v = 1 if v == 0 else v // 1000
-        elif '.' not in stk and 'XAG' not in stk and 'XAU' not in stk:
-            # multiply FX/Money Market prices by 10000
-            o, h, l, c = self.multiply_prices(o, h, l, c, 10000)
-        # all tickers ending in .F are futures, except the LME tickers
-        v = 1 if v == 0 else v
-        # if stk not in db_stx:
-        #     insert_stx = "INSERT INTO equities VALUES "\
-        #                  "('{0:s}', '', 'US Stocks', 'US')".format(stk)
-        #     stxdb.db_write_cmd(insert_stx)
-        db_cmd = "insert into {0:s} values('{1:s}','{2:s}',{3:d},{4:d},"\
-            "{5:d},{6:d},{7:d},{8:d}) on conflict (stk, dt) do update "\
-            "set v={9:d}, oi={10:d}".format(
-                self.eod_tbl, stk, dt, o, h, l, c, v, oi, v, oi)
-        stxdb.db_write_cmd(db_cmd)
-
-    def parseeodfiles(self, s_date, e_date):
-        dt = s_date
-        num_days = stxcal.num_busdays(s_date, e_date)
-        print('Stooq: uploading EOD data for {0:d} days'.format(num_days))
-        day_num = 0
-        while dt <= e_date:
-            print('stooq: {0:s}'.format(dt))
-            # db_stx, _ = self.create_exchange()
-            try:
-                with open('{0:s}/{1:s}_d.prn'.format
-                          (self.in_dir, dt.replace('-', ''))) as ifile:
-                    lines = ifile.readlines()
-            except IOError as ioe:
-                print('{0:s} failed to read EOD file: {1:s}'.
-                      format(dt, str(ioe)))
-                dt = stxcal.next_busday(dt)
-                continue
-            for line in lines:
-                try:
-                    self.parseeodline(line)
-                except Exception as ex:
-                    logging.info('Error with line {0:s}: {1:s}'.format
-                                 (line.strip(), str(ex)))
-            dt = stxcal.next_busday(dt)
-            day_num += 1
-            if day_num % 20 == 0 or day_num == num_days:
-                print(' Uploaded EOD data for {0:d} days'.format(day_num))
-
     def handle_splits(self, start_date):
         split_files = [x for x in os.listdir(self.in_dir)
                        if x.startswith('splits_')]
@@ -269,62 +181,6 @@ class StxDatafeed:
                       'error {2:s}'.format(stk, dt, str(ex)))
         print('Successfully uploaded {0:d} out of {1:d} stock splits'.
               format(num, len(lines)))
-
-    def get_indices(self):
-        indices = {'^DJI': '^DJI', '^SPX': '^SPX', '^NDQ': '^COMP'}
-        end_date = stxcal.move_busdays(str(datetime.datetime.now().date()), 0)
-        for stooq_name, db_name in indices.items():
-            db_cmd = "select max(date) from eods where stk='{0:s}' and " \
-                     "volume > 1".format(db_name)
-            res_db = stxdb.db_read_cmd(db_cmd)
-            start_date = stxcal.next_busday(str(res_db[0][0]))
-            res = requests.get(
-                'https://stooq.com/q/d/l?s={0:s}&d1={1:s}&d2={2:s}'.
-                format(stooq_name,
-                       start_date.replace('-', ''),
-                       end_date.replace('-', '')))
-            lines = res.text.split('\n')
-            for line in lines[1:]:
-                tokens = line.split(',')
-            with open('{0:s}.csv'.format(db_name), 'w') as f:
-                f.write(res.text)
-            print('{0:s}: {1:d}'.format(stooq_name, res.status_code))
-
-    def get_data(self, stk):
-        expiries = stxcal.long_expiries()
-        res = requests.get(self.yhoo_url.format(stk, expiries[0]))
-        print('Got data for {0:s}, status code: {1:d}'.
-              format(stk, res.status_code))
-        res_json = json.loads(res.text)
-        res_0 = res_json['optionChain']['result'][0]
-        quote = res_0['quote']
-        v = quote['regularMarketVolume']
-        o = quote['regularMarketOpen']
-        c = quote['regularMarketPrice']
-        hi = quote['regularMarketDayHigh']
-        lo = quote['regularMarketDayLow']
-        dt = datetime.datetime.fromtimestamp(quote['regularMarketTime'])
-        print('{0:s} {1:s} {2:.2f} {3:.2f} {4:.2f} {5:.2f} {6:d}'.
-              format(stk, str(dt.date()), o, hi, lo, c, v))
-        calls = res_0['options'][0]['calls']
-        puts = res_0['options'][0]['puts']
-        for call in calls:
-            ask = call['ask']['raw']
-            bid = call['bid']['raw']
-            strike = call['strike']['raw']
-            cp = 'C'
-            exp = call['expiration']['fmt']
-            print('  {0:s} {1:s} {2:.2f} {3:.2f} {4:.2f}'
-                  .format(cp, exp, strike, bid, ask)) 
-        for put in puts:
-            ask = put['ask']['raw']
-            bid = put['bid']['raw']
-            strike = put['strike']['raw']
-            cp = 'P'
-            exp = put['expiration']['fmt']
-            print('  {0:s} {1:s} {2:.2f} {3:.2f} {4:.2f}'
-                  .format(cp, exp, strike, bid, ask))
-
 
     def get_available_dates(self, file_pattern, last_date):
         file_list = glob.glob(os.path.join(self.in_dir, file_pattern))
@@ -567,25 +423,12 @@ class StxDatafeed:
                            "analysis='eod_datafeed'".format(last_upload_date))
         logging.info('Updated latest eod datafeed date {0:s} in DB'.
                      format(last_uplaod_date))
-# TODO: 
 
-# 1. Work under the assumption that stooq files are in data_d.txt
-#    file, while eoddata files are in the files
-#    {AMEX|NASDAQ|NYSE}_yyyymmdd.txt
-# 2. For the time being, use only stooq EOD prices, do not use eoddata prices.
-# 3. Use eoddata splits to handle splits. Find the code that parses splits
-# 4. Clean up the code, remove unnecessary methods/lines
-# 5. Automate the calculation of the start date from where data should
-#    be uploaded in the database.
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--source', type=Datafeed,
                         choices=list(Datafeed), default=Datafeed.stooq)
-#     parser.add_argument('-s', '--stooq', action='store_true',
-#                         help='Use data from stooq')
-    parser.add_argument('-b', '--batch', action='store_true',
-                        help='Batch upload of data using file copy')
     parser.add_argument('-d', '--data_dir',
                         help='download directory for EOD files',
                         type=str,
@@ -606,35 +449,3 @@ if __name__ == '__main__':
     sdf.handle_splits(start_date)
     sdf.backup_database()
     sys.exit(0)
-#     if args.stooq:
-#         s_date_sq = '2018-03-12'
-#         e_date_sq = '2018-03-29'
-#         sdf.parseeodfiles(s_date_sq, e_date_sq)
-#         sys.exit(0)
-
-    # We have now data from both EODData and Stooq.  I prefer Stooq.
-    # 1. Get the last date for which eod data is available in the database
-    #    from EODData and Sttoq
-    res = stxdb.db_read_cmd("select dt from analyses where "
-                            "analysis='stooq_datafeed'")
-    stooq_last_date = str(res[0][0]) if res else '2000-01-01'
-    res = stxdb.db_read_cmd("select dt from analyses where "
-                            "analysis='eod_datafeed'")
-    eod_last_date = str(res[0][0]) if res else '2000-01-01'
-
-    # 2. Get the most recent date of a contingent block of dates for
-    #    which data is available from EODData and from stooq
-    stooq_dates = sdf.get_available_dates('*_d.txt', stooq_last_date)
-    eod_amex_dates = sdf.get_available_dates('AMEX_*.txt', eod_last_date)
-    eod_nsdq_dates = sdf.get_available_dates('NASDAQ_*.txt', eod_last_date)
-    eod_nyse_dates = sdf.get_available_dates('NYSE_*.txt', eod_last_date)
-    
-    # 2. get the last trading date
-    end_date = stxcal.move_busdays(str(datetime.datetime.now().date()), 0)
-    # 3. Find out if files are available for the dates
-    # 4. if the files are available, upload them
-#     batch_load = True if args.batch else False
-#     sdf.load_eoddata_files(start_date, end_date, batch=batch_load)
-#     res = stxdb.db_read_cmd("select max(dt) from dividends")
-#     start_date = stxcal.next_busday(str(res[0][0]))
-#     sdf.handle_splits(start_date)
