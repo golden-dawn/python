f1cea9e2bc156b6fe9a03960238969e39e2e0438
golden-dawn <constantin_m_adam@yahoo.com>
2021-02-20 15:24:45 -0500

    Update eod.sh to use the latest code

--- eod.sh
+++ eod.sh
@@ -5,9 +5,9 @@ source .env
 source .bash_profile
 source .bash_profile.pysave
 cd ${HOME}/c
-./stx_ana.exe -eod
+./stx_ana_1.exe -eod
 cd ${HOME}/python
 source .env
 export LANG=en_US.UTF-8
-python3 stx_247.py -e -m -s 15
+python3 stx_247.py -e -s 15
 cd ${CRT_DIR}
