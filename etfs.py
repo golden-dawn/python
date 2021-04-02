a66a09e1907488f2212d87007cb3c205c76dca0a
golden-dawn <constantin_m_adam@yahoo.com>
2021-04-01 23:59:26 -0400

    TODO for ETFs

---
+++ etfs.py
@@ -0,0 +1,8 @@
+import requests
+
+# TODO:
+# Iterate through the alphabet letters
+# Get https://etfdb.com/alpha/A/, ... https://etfdb.com/alpha/Z/
+# Get each ETF:
+# https://etfdb.com/etf/AAAU/#etf-ticker-profile
+# etfdb only has top 15 holdings, can I get full list from yhoo or somewhere else
