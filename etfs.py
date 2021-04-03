cedaa2bb869b2be6e1cc9b372bfe8010d7531ae0
golden-dawn <constantin_m_adam@yahoo.com>
2021-04-02 23:56:21 -0400

    TODO for ETFs

--- etfs.py
+++ etfs.py
@@ -6,3 +6,4 @@ import requests
 # Get each ETF:
 # https://etfdb.com/etf/AAAU/#etf-ticker-profile
 # etfdb only has top 15 holdings, can I get full list from yhoo or somewhere else
+# zacks seems to have a list of holdings for each ETF: https://www.zacks.com/funds/etf/ACES/holding
