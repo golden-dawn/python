beff0dcafe21319617ad2e13d924ead0d17836ae
Golden Dawn <constantin_m_adam@yahoo.com>
2021-04-20 03:31:24 +0000

    Changed the source for the env variables

--- eod_cron.sh
+++ eod_cron.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 export CRT_DIR=${PWD}
 cd ${HOME}
-source .env
-source .bash_profile
-source .bash_profile.pysave
+source ${HOME}/.env_vars
+source ${HOME}/.bash_profile
+source ${HOME}/.bash_profile.pysave
 cd ${HOME}/c
 ./stx_ana_1.exe --eod --cron
 cd ${HOME}/python
