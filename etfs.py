43546b87eddbfe630efbbd80ac49e81fee0f8385
Golden Dawn <constantin_m_adam@yahoo.com>
2021-04-26 12:22:43 +0000

    WIP - ETFs

--- etfs.py
+++ etfs.py
@@ -1,3 +1,4 @@
+import os
 import requests
 
 # TODO:
@@ -7,3 +8,15 @@ import requests
 # https://etfdb.com/etf/AAAU/#etf-ticker-profile
 # etfdb only has top 15 holdings, can I get full list from yhoo or somewhere else
 # zacks seems to have a list of holdings for each ETF: https://www.zacks.com/funds/etf/ACES/holding
+
+filename = os.path.join(os.getenv('HOME'), 'want.html')
+with open(filename, 'r') as f:
+    text = f.read()
+lines = text.split('\n')
+holdings_line = ''
+for line in lines:
+    if line.startswith('etf_holdings.formatted_data'):
+        holdings_line = line
+holdings_tokens = holdings_line[34: -5].split(' ] ,  [ ')
+# >>> holdings_tokens[5]
+# '"AMAZON.COM INC", "<button class=\\"modal_external appear-on-focus\\" href=\\"\\/modals\\/quick-quote.php\\" rel=\\"AMZN\\">AMZN Quick Quote<\\/button><a href=\\"\\/\\/www.zacks.com\\/funds\\/etf\\/AMZN\\" rel=\\"AMZN\\" class=\\" hoverquote-container-od \\" show-add-portfolio=\\"true\\" ><span class=\\"hoverquote-symbol\\">AMZN<span class=\\"sr-only\\"><\\/span><\\/span><\\/a>", "1,972", "4.36", "41.35", "<a class=\\"report_document newwin\\" href=\\"/zer/report/AMZN\\" alt=\\"View Report\\" title=\\"View Report\\"></a>"'
