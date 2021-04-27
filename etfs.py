fd29b421880d47fe8d6d13e008891c42a35b1465
Golden Dawn <constantin_m_adam@yahoo.com>
2021-04-27 07:38:53 +0000

    WIP: get ETF holdings - parse ETF page

--- etfs.py
+++ etfs.py
@@ -1,14 +1,23 @@
 import os
 import requests
+import string
 
 # TODO:
 # Iterate through the alphabet letters
 # Get https://etfdb.com/alpha/A/, ... https://etfdb.com/alpha/Z/
+# To get the next page do:
+# https://etfdb.com/alpha/B/#etfs&sort_name=symbol&sort_order=asc&page=2
 # Get each ETF:
 # https://etfdb.com/etf/AAAU/#etf-ticker-profile
 # etfdb only has top 15 holdings, can I get full list from yhoo or somewhere else
 # zacks seems to have a list of holdings for each ETF: https://www.zacks.com/funds/etf/ACES/holding
 
+
+for letter in list(string.ascii_uppercase):
+    etf_url = f'https://etfdb.com/alpha/{letter}/'
+
+
+# begin - parse the holdings info for each fund
 filename = os.path.join(os.getenv('HOME'), 'want.html')
 with open(filename, 'r') as f:
     text = f.read()
@@ -18,5 +27,19 @@ for line in lines:
     if line.startswith('etf_holdings.formatted_data'):
         holdings_line = line
 holdings_tokens = holdings_line[34: -5].split(' ] ,  [ ')
-# >>> holdings_tokens[5]
-# '"AMAZON.COM INC", "<button class=\\"modal_external appear-on-focus\\" href=\\"\\/modals\\/quick-quote.php\\" rel=\\"AMZN\\">AMZN Quick Quote<\\/button><a href=\\"\\/\\/www.zacks.com\\/funds\\/etf\\/AMZN\\" rel=\\"AMZN\\" class=\\" hoverquote-container-od \\" show-add-portfolio=\\"true\\" ><span class=\\"hoverquote-symbol\\">AMZN<span class=\\"sr-only\\"><\\/span><\\/span><\\/a>", "1,972", "4.36", "41.35", "<a class=\\"report_document newwin\\" href=\\"/zer/report/AMZN\\" alt=\\"View Report\\" title=\\"View Report\\"></a>"'
+
+hold_list = []
+
+for holding_row in holdings_tokens:
+    holding_tokens = holding_row.split(', ')
+    ticker_token = holding_tokens[1]
+    ticker_index = ticker_token.find('rel=')
+    if ticker_index == -1:
+        continue
+    ticker_tokens = ticker_token[ticker_index:].split('\\"')
+    if len(ticker_tokens) >= 2:
+        ticker = ticker_tokens[1]
+        hold_list.append(ticker)
+    
+print(f'ETF WANT has {len(hold_list)} equity holdings: {hold_list}')
+# end - parse the holdings info for each fund
