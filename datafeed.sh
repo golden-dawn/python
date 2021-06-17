15caccb2cfc66a33017334fce00562504cba5730
golden-dawn <constantin_m_adam@yahoo.com>
2021-06-17 11:32:33 -0400

    Run script in stx python virtual env

--- datafeed.sh
+++ datafeed.sh
@@ -2,10 +2,8 @@
 export CRT_DIR=${PWD}
 cd ${HOME}
 source ${HOME}/.env_vars
-source ${HOME}/.bash_profile
-source ${HOME}/.bash_profile.pysave
 cd ${HOME}/python
-source .env
+source ${HOME}/.envs/stx/bin/activate
 export LANG=en_US.UTF-8
 python3 stx_datafeed.py
 cd ${CRT_DIR}
