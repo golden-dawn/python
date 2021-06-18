aa0cb398713960000f16c8e7f63533bdb025c6f2
golden-dawn <constantin_m_adam@yahoo.com>
2021-06-18 11:33:39 -0400

    Run script in stx python virtual env

--- intraday_cron.sh
+++ intraday_cron.sh
@@ -2,12 +2,10 @@
 export CRT_DIR=${PWD}
 cd ${HOME}
 source ${HOME}/.env_vars
-source ${HOME}/.bash_profile
-source ${HOME}/.bash_profile.pysave
 cd ${HOME}/c
 ./stx_ana_1.exe --intraday --cron
 cd ${HOME}/python
-source .env
+source ${HOME}/.envs/stx/bin/activate
 export LANG=en_US.UTF-8
 python3 stx_247.py -i -c -s 15 
 cd ${CRT_DIR}
