0d9bfb8f0b801c5bc47be1c2a66fff3c36ddfb95
Golden Dawn <constantin_m_adam@yahoo.com>
2021-06-23 15:46:27 -0400

    Import GoogleDriveClient

--- stx_247.py
+++ stx_247.py
@@ -14,6 +14,7 @@ import stxdb
 from stxjl import StxJL
 from stxts import StxTS
 from stx_plot import StxPlot
+from stx_storage import GoogleDriveClient
 import sys
 import time
 import traceback as tb
